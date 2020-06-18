2e51a0b510fe0a2138ac73a85a2bf4485dc46667
jgaffuri <jgaf@protonmail.com>
2020-06-18 11:09:06 +0200

    base training

--- trainingW3C/13_ml.py
+++ trainingW3C/13_ml.py
@@ -86,5 +86,105 @@ print(r2_score(y, mymodel(x)))
 
 # Multiple Regression
 
+import pandas
+
+df = pandas.read_csv("trainingW3C/cars.csv")
+#print(df)
+X = df[['Weight', 'Volume']]
+y = df['CO2']
+
+from sklearn import linear_model
+regr = linear_model.LinearRegression()
+regr.fit(X, y)
+
+print(regr.coef_)
+
+#predict the CO2 emission of a car where the weight is 2300kg, and the volume is 1300ccm:
+predictedCO2 = regr.predict([[2300, 1300]])
+
+print(predictedCO2)
+
+
+
+
+
+# train/test
+
+numpy.random.seed(2)
+
+x = numpy.random.normal(3, 1, 100)
+y = numpy.random.normal(150, 40, 100) / x
+#plt.scatter(x, y)
+#plt.show()
+
+
+train_x = x[:80]
+train_y = y[:80]
+#plt.scatter(train_x, train_y)
+#plt.show()
+
+test_x = x[80:]
+test_y = y[80:]
+#plt.scatter(test_x, test_y)
+#plt.show()
+
+
+mymodel = numpy.poly1d(numpy.polyfit(train_x, train_y, 4))
+
+#plt.scatter(train_x, train_y)
+#myline = numpy.linspace(0, 6, 100)
+#plt.plot(myline, mymodel(myline))
+#plt.show()
+
+r2 = r2_score(train_y, mymodel(train_x))
+print(r2)
+
+r2 = r2_score(test_y, mymodel(test_x))
+print(r2)
+
+
+
+
+
+
+
+# Decision Tree
+
+
+
+import pandas
+from sklearn import tree
+import pydotplus
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+import matplotlib.image as pltimg
+
+df = pandas.read_csv("trainingW3C/shows.csv")
+#print(df)
+
+d = {'UK': 0, 'USA': 1, 'N': 2}
+df['Nationality'] = df['Nationality'].map(d)
+d = {'YES': 1, 'NO': 0}
+df['Go'] = df['Go'].map(d)
+
+#print(df)
+
+
+features = ['Age', 'Experience', 'Rank', 'Nationality']
+X = df[features]
+y = df['Go']
+#print(X)
+#print(y)
+
+
+dtree = DecisionTreeClassifier()
+dtree = dtree.fit(X, y)
+data = tree.export_graphviz(dtree, out_file=None, feature_names=features)
+graph = pydotplus.graph_from_dot_data(data)
+graph.write_png('trainingW3C/mydecisiontree.png')
+
+img = pltimg.imread('trainingW3C/mydecisiontree.png')
+imgplot = plt.imshow(img)
+plt.show()
 
 
