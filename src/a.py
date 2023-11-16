8737e45fd299593fda530b91acda2275c8847bf7
jgaffuri <jgaf@proton.me>
2023-11-16 12:33:27 +0100

    a

--- src/a.py
+++ src/a.py
@@ -1,13 +1,19 @@
 import pandas as pd
 
-file_path = '/home/juju/Bureau/gisco/grid_pop_c2021/NL_2021_0000_V0002.csv'
-df = pd.read_csv(file_path, sep=';')
+rep="/home/juju/Bureau/gisco/grid_pop_c2021/"
 
-df = df[["STAT","SPATIAL","OBS_VALUE"]]
+for cc in ['NL','AT']:
+    print(cc)
+    file_path = rep + cc+'_in.csv'
+    df = pd.read_csv(file_path, sep=';')
 
-#print(df['STAT'].unique())
-# ['CHG_IN' 'CHG_OUT' 'EMP' 'EU_OTH' 'F' 'M' 'NAT' 'OTH' 'SAME' 'T' 'Y_GE65' 'Y_LT15' 'Y15-64']
+    df = df[["STAT","SPATIAL","OBS_VALUE"]]
 
-df = df.pivot(index='SPATIAL', columns='STAT', values='OBS_VALUE')
+    #print(df['STAT'].unique())
+    # ['CHG_IN' 'CHG_OUT' 'EMP' 'EU_OTH' 'F' 'M' 'NAT' 'OTH' 'SAME' 'T' 'Y_GE65' 'Y_LT15' 'Y15-64']
 
-print(df)
+    df = df.pivot(index='SPATIAL', columns='STAT', values='OBS_VALUE')
+
+    #print(df)
+
+    df.to_csv(rep+cc+".csv", index=True)
