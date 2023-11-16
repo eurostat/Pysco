e6483bb5ea2aac51e54ac5e84f50a3977c9d9f33
jgaffuri <jgaf@proton.me>
2023-11-16 13:37:58 +0100

    a

--- src/a.py
+++ src/a.py
@@ -1,9 +1,7 @@
 import pandas as pd
 
-rep="/home/juju/Bureau/gisco/grid_pop_c2021/"
-ccs = ['NL','AT']
 
-for cc in ccs:
+def prepare(rep, cc):
     print(cc)
     file_path = rep + cc+'_in.csv'
     df = pd.read_csv(file_path, sep=';')
@@ -22,8 +20,18 @@ for cc in ccs:
 
     df.to_csv(rep+cc+".csv", index=True)
 
+def merge(rep, ccs):
+    print('merge '+str(ccs))
+    dfs = [pd.read_csv(rep+cc+".csv") for cc in ccs]
+    merged_df = pd.concat(dfs, axis=0, ignore_index=True)
+    merged_df.to_csv(rep+"EU.csv", index=False)
+   
 
-#merge
-dfs = [pd.read_csv(rep+cc+".csv") for cc in ccs]
-merged_df = pd.concat(dfs, axis=0, ignore_index=True)
-merged_df.to_csv(rep+"EU.csv", index=False)
+
+
+rep="/home/juju/Bureau/gisco/grid_pop_c2021/"
+ccs = ['NL','AT']
+
+#for cc in ccs: prepare(rep, cc)
+#merge(rep, ccs)
+
