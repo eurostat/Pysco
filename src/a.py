404fe026addebe62244479d957f3dab966c6e0dd
jgaffuri <jgaf@proton.me>
2023-11-16 12:22:52 +0100

    a

--- src/a.py
+++ src/a.py
@@ -8,5 +8,6 @@ df = df[["STAT","SPATIAL","OBS_VALUE"]]
 #print(df['STAT'].unique())
 # ['CHG_IN' 'CHG_OUT' 'EMP' 'EU_OTH' 'F' 'M' 'NAT' 'OTH' 'SAME' 'T' 'Y_GE65' 'Y_LT15' 'Y15-64']
 
+df = df.pivot(index='SPATIAL', columns='STAT', values='OBS_VALUE')
 
-#print(df)
+print(df)
