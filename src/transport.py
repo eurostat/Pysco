63ebeec6888339c64a53ce083fb8571f85f7452e
jgaffuri <jgaf@proton.me>
2024-03-01 17:47:43 +0100

    a

--- src/transport.py
+++ src/transport.py
@@ -5,12 +5,11 @@ from shapely.geometry import LineString
 file = "/home/juju/Bureau/gisco/rail_rinf/NET_SEGMENTS_EU_EFTA.xlsx"
 outFolder = '/home/juju/Bureau/gisco/rail_rinf/'
 
-print("Start")
-
-# df = pd.read_csv('jhbb.csv')
+print("Load data from "+file)
 df = pd.read_excel(file)
 print(str(len(df)) + " segments loaded")
 
+print("Make features")
 geometries = []
 for index, row in df.iterrows():
     try:
@@ -19,11 +18,9 @@ for index, row in df.iterrows():
         point2 = (row['Tolongitude'], row['Tolatitude'])
         geometries.append(LineString([point1, point2]))
     except Exception as e:
-        print("An error occurred for segment "+row["Network segment identifier"]+":", e)
+        print("Problem with segment "+row["Network segment identifier"]+":", e)
         geometries.append(LineString([]))
 
 print("Save as GPKG file")
 gdf = gpd.GeoDataFrame(df, geometry=geometries)
 gdf.to_file(outFolder+'out.gpkg', driver='GPKG', crs="EPSG:4258")
-
-print("End")
