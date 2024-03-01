f1f963d331f17a2d31baa650f6a6f4ea4f72a9ce
jgaffuri <jgaf@proton.me>
2024-03-01 17:44:22 +0100

    a

--- src/transport.py
+++ src/transport.py
@@ -5,17 +5,25 @@ from shapely.geometry import LineString
 file = "/home/juju/Bureau/gisco/rail_rinf/NET_SEGMENTS_EU_EFTA.xlsx"
 outFolder = '/home/juju/Bureau/gisco/rail_rinf/'
 
-# df = pd.read_csv('your_csv_file.csv')
-df = pd.read_excel(file)
+print("Start")
 
-print(df)
+# df = pd.read_csv('jhbb.csv')
+df = pd.read_excel(file)
+print(str(len(df)) + " segments loaded")
 
 geometries = []
 for index, row in df.iterrows():
-    point1 = (row['x1'], row['y1'])
-    point2 = (row['x2'], row['y2'])
-    line = LineString([point1, point2])
-    geometries.append(line)
+    try:
+        # print(row['Fromlongitude'], row['Fromlatitude'], row['Tolongitude'], row['Tolatitude'])
+        point1 = (row['Fromlongitude'], row['Fromlatitude'])
+        point2 = (row['Tolongitude'], row['Tolatitude'])
+        geometries.append(LineString([point1, point2]))
+    except Exception as e:
+        print("An error occurred for segment "+row["Network segment identifier"]+":", e)
+        geometries.append(LineString([]))
 
+print("Save as GPKG file")
 gdf = gpd.GeoDataFrame(df, geometry=geometries)
-gdf.to_file(outFolder+'out.gpkg', driver='GPKG')
+gdf.to_file(outFolder+'out.gpkg', driver='GPKG', crs="EPSG:4258")
+
+print("End")
