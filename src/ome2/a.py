5923fab4b636692e078275856a0388c650e91830
jgaffuri <jgaf@proton.me>
2024-03-27 10:56:45 +0100

    a

--- src/ome2/a.py
+++ src/ome2/a.py
@@ -1,19 +1,29 @@
+import fiona
 import geopandas as gpd
 from shapely.geometry import box
 
 # reproject
 # ogr2ogr -f GPKG -t_srs EPSG:3035 ome2.gpkg ome2_hvlsp_v1.1.1.gpkg
 
-file_path = '/home/juju/Bureau/gisco/geodata/OME2_HVLSP_v1/gpkg/ome2_hvlsp_v1.1.1.gpkg'
+file_path = '/home/juju/Bureau/gisco/geodata/OME2_HVLSP_v1/gpkg/ome2.gpkg'
 out_folder = '/home/juju/Bureau/gisco/OME2_analysis/'
-minx = 2720000
-maxx = 2730000
-miny = 4080000
-maxy = 4090000
+minx = 3920000
+maxx = 3930000
+miny = 2990000
+maxy = 3000000
 bbox = box(minx, miny, maxx, maxy)
 
 print("loading...")
-gdf = gpd.read_file(file_path, bbox=bbox) #, layer='batiment'
+gdf = gpd.read_file(file_path, layer='tn_road_link')#, bbox=bbox
 
+print(len(gdf))
+print(gdf.bounds)
 print(gdf.dtypes)
 print(gdf.shape)
+
+
+
+#netwrok analysis libs:
+#NetworkX
+#igraph: C with interfaces for Python
+#Graph-tool: implemented in C++ with a Python interface
