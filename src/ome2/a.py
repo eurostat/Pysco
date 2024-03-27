dd7ec374180c0633581069ff868763d37d95949c
jgaffuri <jgaf@proton.me>
2024-03-27 10:28:18 +0100

    a

--- src/ome2/a.py
+++ src/ome2/a.py
@@ -1,7 +1,11 @@
 import geopandas as gpd
 from shapely.geometry import box
 
+# reproject
+# ogr2ogr -f GPKG -t_srs EPSG:3035 ome2.gpkg ome2_hvlsp_v1.1.1.gpkg
+
 file_path = '/home/juju/Bureau/gisco/geodata/OME2_HVLSP_v1/gpkg/ome2_hvlsp_v1.1.1.gpkg'
+out_folder = '/home/juju/Bureau/gisco/OME2_analysis/'
 minx = 2720000
 maxx = 2730000
 miny = 4080000
