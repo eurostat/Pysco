2759a0111f562a3a44ff9fbcee72d5e19308c446
jgaffuri <jgaf@proton.me>
2024-03-27 11:11:08 +0100

    a

--- src/ome2/a.py
+++ src/ome2/a.py
@@ -7,23 +7,26 @@ from shapely.geometry import box
 
 file_path = '/home/juju/Bureau/gisco/geodata/OME2_HVLSP_v1/gpkg/ome2.gpkg'
 out_folder = '/home/juju/Bureau/gisco/OME2_analysis/'
-minx = 3920000
-maxx = 3930000
-miny = 2990000
-maxy = 3000000
+minx = 3890000
+maxx = 3940000
+miny = 2980000
+maxy = 3030000
 bbox = box(minx, miny, maxx, maxy)
 
 print("loading...")
-gdf = gpd.read_file(file_path, layer='tn_road_link')#, bbox=bbox
-
+gdf = gpd.read_file(file_path, layer='tn_road_link', bbox=bbox)
 print(len(gdf))
-print(gdf.bounds)
-print(gdf.dtypes)
-print(gdf.shape)
 
+#print(gdf.bounds)
+#print(gdf.dtypes)
+#print(gdf.shape)
+
+print("save...")
+gdf.to_file(out_folder+"test.gpkg", driver="GPKG")
 
 
 #netwrok analysis libs:
 #NetworkX
 #igraph: C with interfaces for Python
 #Graph-tool: implemented in C++ with a Python interface
+
