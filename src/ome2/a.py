ecd638dcde0370ff51534f8f8f071aebdbdb0f9e
jgaffuri <jgaf@proton.me>
2024-03-27 10:21:39 +0100

    a

--- src/ome2/a.py
+++ src/ome2/a.py
@@ -1,7 +1,7 @@
 import geopandas as gpd
 from shapely.geometry import box
 
-file_path = 'data/90.gpkg'
+file_path = '/home/juju/Bureau/gisco/geodata/OME2_HVLSP_v1/gpkg/ome2_hvlsp_v1.1.1.gpkg'
 minx = 2720000
 maxx = 2730000
 miny = 4080000
@@ -9,7 +9,7 @@ maxy = 4090000
 bbox = box(minx, miny, maxx, maxy)
 
 print("loading...")
-gdf = gpd.read_file(file_path, layer='batiment', bbox=bbox)
+gdf = gpd.read_file(file_path, bbox=bbox) #, layer='batiment'
 
 print(gdf.dtypes)
 print(gdf.shape)
