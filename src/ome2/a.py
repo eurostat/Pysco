8e8cf325d4d76e84132938c496f7c649e73785d6
jgaffuri <jgaf@proton.me>
2024-03-27 11:51:40 +0100

    a

--- src/ome2/a.py
+++ src/ome2/a.py
@@ -1,14 +1,28 @@
 import geopandas as gpd
-from shapely.geometry import box
+import networkx as nx
 
 out_folder = '/home/juju/Bureau/gisco/OME2_analysis/'
 
-print("loading...")
-gdf = gpd.read_file(out_folder+"test.gpkg")
-print(len(gdf))
-
 #network analysis libs:
-#NetworkX
-#igraph: C with interfaces for Python
-#Graph-tool: implemented in C++ with a Python interface
+#NetworkX: https://networkx.org/documentation/stable/reference/algorithms/shortest_paths.html
+#igraph: C with interfaces for Python. https://python.igraph.org/en/stable/api/igraph.GraphBase.html#get_shortest_path
+#Graph-tool: implemented in C++ with a Python interface. https://graph-tool.skewed.de/static/doc/topology.html
+
+
+#print("loading...")
+#gdf = gpd.read_file(out_folder+"test.gpkg")
+#print(len(gdf))
+
+
+graph = nx.Graph()
+graph.add_edge("A", "B", weight=4)
+graph.add_edge("B", "D", weight=2)
+graph.add_edge("A", "C", weight=3)
+graph.add_edge("C", "D", weight=4)
+sp = nx.shortest_path(graph, "A", "D", weight="weight")
+print(sp)
+
+#go through links. initial and final point. build node id with rounded x/y coordinates. add edge.
+#comput weight
+#link edge to feature/geometry
 
