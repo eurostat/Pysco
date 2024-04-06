515ef8e3fd029b4f38957e1dea64e43c12a44a4b
jgaffuri <jgaf@proton.me>
2024-04-06 21:55:24 +0200

    a

--- src/ome2/a.py
+++ src/ome2/a.py
@@ -14,43 +14,12 @@ xMin = 3900000
 yMin = 3000000 
 size = 60000
 resolution = 1000
-gdf = gpd.read_file(out_folder+"test_"+str(size)+".gpkg") #, query="form_of_way != 'tractor_road'")
-gdf = gdf[gdf['road_surface_category'] != 'unpaved']
-gdf = gdf[gdf['road_surface_category'] != 'paved#unpaved']
-gdf = gdf[gdf['road_surface_category'] != 'unpaved#paved']
-gdf = gdf[gdf['form_of_way'] != 'tractor_road']
-gdf = gdf[gdf['form_of_way'] != 'tractor_road#single_carriage_way']
-gdf = gdf[gdf['form_of_way'] != 'single_carriage_way#tractor_road']
-gdf = gdf[gdf['access_restriction'] != 'physically_impossible']
-gdf = gdf[gdf['access_restriction'] != 'private']
-gdf = gdf[gdf['access_restriction'] != 'void_restricted#private']
-gdf = gdf[gdf['condition_of_facility'] != 'disused']
-gdf = gdf[gdf['condition_of_facility'] != 'under_construction']
+gdf = gpd.read_file(out_folder+"test_"+str(size)+"_prepared.gpkg")
 print(str(len(gdf)) + " links")
 #print(gdf.dtypes)
 
-#define weight function
-def weight_function(f):
-    fow = f["form_of_way"]
-    frc = f["functional_road_class"]
-    speed_kmh = 30
-    if(fow == 'motorway'): speed_kmh = 120
-    elif(fow == 'dual_carriage_way'): speed_kmh = 100
-    elif(fow == 'slip_road'): speed_kmh = 80
-    #elif(fow == 'single_carriage_way'): speed_kmh = 80
-    elif(frc == 'main_road'): speed_kmh = 80
-    elif(frc == 'first_class'): speed_kmh = 80
-    elif(frc == 'second_class'): speed_kmh = 70
-    elif(frc == 'third_class'): speed_kmh = 50
-    elif(frc == 'fourth_class'): speed_kmh = 40
-    elif(frc == 'fifth_class'): speed_kmh = 30
-    elif(frc == 'void_unk'): speed_kmh = 30
-    else: print(fow,frc)
-    return round(f.geometry.length / speed_kmh * 3.6)
-
 print(datetime.now(), "make graph")
-graph = graph_from_geodataframe(gdf, weight_function)
-
+graph = graph_from_geodataframe(gdf, lambda f:f.duration)
 #clear memory
 del gdf
 
