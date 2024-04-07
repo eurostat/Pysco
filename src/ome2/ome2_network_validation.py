329bd0cc4e282ebe8b21934e4aed837772a2d8dc
jgaffuri <jgaf@proton.me>
2024-04-07 16:31:46 +0200

    a

--- src/ome2/ome2_network_validation.py
+++ src/ome2/ome2_network_validation.py
@@ -33,11 +33,20 @@ for i in range(nbx):
     for j in range(nby):
         bbox = [xmin+i*window,ymin+j*window,xmin+(i+1)*window,ymin+(j+1)*window]
         print("******" ,bbox)
+
         print(datetime.now(), "load nodes")
         nodes = gpd.read_file(folder+"xborder_nodes_stamped.gpkg", bbox=bbox)
         print(len(nodes))
         if(len(nodes)==0): continue
+
+        nodes1 = nodes[nodes['country_id'] == cnt1]
+        nodes2 = nodes[nodes['country_id'] == cnt2]
+        print(len(nodes1), len(nodes2))
+        if(len(nodes1)==0): continue
+        if(len(nodes2)==0): continue
+
         print(datetime.now(), "load network links")
         rn = gpd.read_file(file_path, layer='tn_road_link', bbox=bbox)
         print(len(rn))
 
+        
