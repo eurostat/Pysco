02e49001e0b4f3a3b5e03bfce6f924d9ce5d9b9e
jgaffuri <jgaf@proton.me>
2024-04-09 20:25:04 +0200

    a

--- src/ome2/ome2_network_validation.py
+++ src/ome2/ome2_network_validation.py
@@ -5,6 +5,14 @@ import math
 from netutils import shortest_path_geometry,graph_from_geodataframe,nodes_spatial_index,a_star_euclidian_dist
 from ome2utils import ome2_filter_road_links
 import networkx as nx
+import concurrent.futures
+
+
+def cartesian_product(nb1, nb2):
+    pairs = []
+    for i in range(nb1 + 1):
+        for j in range(nb2 + 1): pairs.append([i, j])
+    return pairs
 
 folder = '/home/juju/Bureau/gisco/OME2_analysis/'
 file_path = '/home/juju/Bureau/gisco/geodata/OME2_HVLSP_v1/gpkg/ome2.gpkg'
@@ -35,71 +43,75 @@ def validation(cnt1,cnt2):
     #output paths
     sp_geometries = []
 
-    #iterate through partition
-    for i in range(nbx):
-        for j in range(nby):
-            bbox = [xmin+i*window-window_margin, ymin+j*window-window_margin, xmin+(i+1)*window+window_margin, ymin+(j+1)*window+ window_margin]
-            print("******" ,bbox)
+    #parallel function
+    def pfun(p):
+        [i,j] = p
+        bbox = [xmin+i*window-window_margin, ymin+j*window-window_margin, xmin+(i+1)*window+window_margin, ymin+(j+1)*window+ window_margin]
+        print("******" ,bbox)
 
-            print(datetime.now(), "load nodes")
-            nodes = gpd.read_file(folder+"xborder_nodes_stamped.gpkg", bbox=bbox)
-            #print(len(nodes))
-            if(len(nodes)==0): continue
+        print(datetime.now(), "load nodes")
+        nodes = gpd.read_file(folder+"xborder_nodes_stamped.gpkg", bbox=bbox)
+        #print(len(nodes))
+        if(len(nodes)==0): return
 
-            #filter nodes per country
-            nodes1 = nodes[nodes['country_id'] == cnt1]
-            nodes2 = nodes[nodes['country_id'] == cnt2]
-            del nodes
-            #print(len(nodes1), len(nodes2))
-            if(len(nodes1)==0): continue
-            if(len(nodes2)==0): continue
+        #filter nodes per country
+        nodes1 = nodes[nodes['country_id'] == cnt1]
+        nodes2 = nodes[nodes['country_id'] == cnt2]
+        del nodes
+        #print(len(nodes1), len(nodes2))
+        if(len(nodes1)==0): return
+        if(len(nodes2)==0): return
 
-            print(datetime.now(), "load network links")
-            rn = gpd.read_file(file_path, layer='tn_road_link', bbox=bbox)
-            #print(len(rn))
-            if(len(rn)==0): continue
+        print(datetime.now(), "load network links")
+        rn = gpd.read_file(file_path, layer='tn_road_link', bbox=bbox)
+        #print(len(rn))
+        if(len(rn)==0): return
 
-            print(datetime.now(), "filter network links")
-            rn = ome2_filter_road_links(rn)
-            #print(len(rn))
-            if(len(rn)==0): continue
+        print(datetime.now(), "filter network links")
+        rn = ome2_filter_road_links(rn)
+        #print(len(rn))
+        if(len(rn)==0): return
 
-            print(datetime.now(), "make graph")
-            graph = graph_from_geodataframe(rn, lambda f:f.geometry.length)
-            del rn
+        print(datetime.now(), "make graph")
+        graph = graph_from_geodataframe(rn, lambda f:f.geometry.length)
+        del rn
 
-            print(datetime.now(), "make list of nodes")
-            nodes_ = []
-            for node in graph.nodes(): nodes_.append(node)
+        print(datetime.now(), "make list of nodes")
+        nodes_ = []
+        for node in graph.nodes(): nodes_.append(node)
 
-            print(datetime.now(), "make nodes spatial index")
-            idx = nodes_spatial_index(graph)
+        print(datetime.now(), "make nodes spatial index")
+        idx = nodes_spatial_index(graph)
 
-            print(datetime.now(), "compute paths")
-            for iii,n1 in nodes1.iterrows():
-                #get country 1 node
-                n1_ = nodes_[next(idx.nearest((n1.geometry.x, n1.geometry.y, n1.geometry.x, n1.geometry.y), 1))]
-                for jjj,n2 in nodes2.iterrows():
+        print(datetime.now(), "compute paths")
+        for iii,n1 in nodes1.iterrows():
+            #get country 1 node
+            n1_ = nodes_[next(idx.nearest((n1.geometry.x, n1.geometry.y, n1.geometry.x, n1.geometry.y), 1))]
+            for jjj,n2 in nodes2.iterrows():
 
-                    #skip node pairs too far away
-                    d = n1.geometry.distance(n2.geometry)
-                    if(d > distance_threshold): continue
+                #skip node pairs too far away
+                d = n1.geometry.distance(n2.geometry)
+                if(d > distance_threshold): continue
 
-                    #get country 2 node
-                    n2_ = nodes_[next(idx.nearest((n2.geometry.x, n2.geometry.y, n2.geometry.x, n2.geometry.y), 1))]
+                #get country 2 node
+                n2_ = nodes_[next(idx.nearest((n2.geometry.x, n2.geometry.y, n2.geometry.x, n2.geometry.y), 1))]
 
-                    #may happen (?)
-                    if(n1_==n2_): continue
+                #may happen (?)
+                if(n1_==n2_): continue
 
-                    try:
-                        #compute shortest path
-                        sp = nx.astar_path(graph, n1_, n2_, heuristic=a_star_euclidian_dist, weight="weight")
-                        line = shortest_path_geometry(sp)
-                        sp_geometries.append(line)
-                    except nx.NetworkXNoPath as e: pass#print("Exception NetworkXNoPath:", e)
-                    except GEOSException as e: print("Exception GEOSException:", e)
+                try:
+                    #compute shortest path
+                    sp = nx.astar_path(graph, n1_, n2_, heuristic=a_star_euclidian_dist, weight="weight")
+                    line = shortest_path_geometry(sp)
+                    sp_geometries.append(line)
+                except nx.NetworkXNoPath as e: pass#print("Exception NetworkXNoPath:", e)
+                except GEOSException as e: print("Exception GEOSException:", e)
 
-            print(datetime.now(), len(sp_geometries), "paths")
+        print(datetime.now(), len(sp_geometries), "paths")
+
+    #launch parallel computation   
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(pfun, cartesian_product(nbx,nby))
 
 
     if(len(sp_geometries)==0): exit()
