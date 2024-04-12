dbbb73aa9bfcac716b6312ae5e64ad5b4c26d5bf
jgaffuri <jgaf@proton.me>
2024-04-12 17:43:59 +0200

    a

--- src/ome2/b.py
+++ src/ome2/b.py
@@ -1,8 +1,21 @@
+import geopandas as gpd
 from geopandas.tools import overlay
+from datetime import datetime
 
+folder = '/home/juju/Bureau/gisco/OME2_analysis/'
+file_path = '/home/juju/Bureau/gisco/geodata/OME2_HVLSP_v1/gpkg/ome2.gpkg'
 
+print(datetime.now(), "load paths")
+paths = gpd.read_file(folder+"ome2_validation_paths_be_fr.gpkg")
+print(str(len(paths)), "paths")
 
+print(datetime.now(), "load boundaries")
+borders = gpd.read_file(folder+"bnd.gpkg")
+print(str(len(borders)), "boundaries")
 
+print(datetime.now(), "intersection")
+intersection = overlay(borders, paths, how='intersection', keep_geom_type=False)
+print(str(len(intersection)), "intersections")
 
-intersection = overlay(gdf1, gdf2, how='intersection')
-
+print(datetime.now(), "save intersections")
+intersection.to_file(folder+"intersections.gpkg", driver="GPKG")
