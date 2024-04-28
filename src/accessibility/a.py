9b32ff68b96fc78ede4863782ee2368c6b60ab51
jgaffuri <jgaf@proton.me>
2024-04-28 21:22:11 +0200

    a

---
+++ src/accessibility/a.py
@@ -0,0 +1,14 @@
+
+
+#define 50km partition
+
+#make 100km window around
+
+#get road network in it
+#get hospitals in it
+
+#get populated grid cells in 50km partition
+
+#for each grid cell, get 5 hospitals around - compute shortest path to nearest
+#OR
+
