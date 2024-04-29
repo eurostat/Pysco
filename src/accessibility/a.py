6a9de2929781cf3a9bae456177322eebd6fb8998
jgaffuri <jgaf@proton.me>
2024-04-29 09:50:15 +0200

    a

--- src/accessibility/a.py
+++ src/accessibility/a.py
@@ -1,8 +1,16 @@
+from shapely.geometry import box
 
 
 #define 50km partition
+partition_size = 50000
+x_part = 3950000
+y_part = 2850000
+partition_bbox = box(x_part, y_part, x_part+partition_size, y_part+partition_size)
 
-#make 100km window around
+
+#make buffered window around
+extended_bbox = box(x_part-partition_size*0.5, y_part-partition_size*0.5, x_part+partition_size*1.5, y_part+partition_size*1.5)
+
 
 #get road network in it
 #get hospitals in it
