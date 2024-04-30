126d477c62bfb6d7ca3384ac5d5e8d3d0d77e307
jgaffuri <jgaf@proton.me>
2024-04-30 10:18:23 +0200

    a

---
+++ src/OSM2network.py
@@ -0,0 +1,13 @@
+
+
+#Highway tag
+#https://wiki.openstreetmap.org/wiki/Key:highway
+#motorway, trunk, primary, secondary, tertiary, residential, service, etc.
+
+#attributes:
+#maxspeed
+#lanes
+#oneway
+#smoothness
+#surface
+#access
