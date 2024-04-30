7cf4a16a9eaedc2bcb9aa2ca0903070606967e23
jgaffuri <jgaf@proton.me>
2024-04-30 10:55:40 +0200

    a

--- src/OSM2network.py
+++ src/OSM2network.py
@@ -1,8 +1,22 @@
+import geopandas as gpd
+from datetime import datetime
 
 
-#Highway tag
-#https://wiki.openstreetmap.org/wiki/Key:highway
-#motorway, trunk, primary, secondary, tertiary, residential, service, etc.
+
+osm_file = "/home/juju/geodata/OSM/europe.gpkg"
+
+
+print(datetime.now(), "load OSM lines")
+rn = gpd.read_file(osm_file, layer='lines')
+print(str(len(rn)), "lines")
+
+print(datetime.now(), "filter highway != NULL")
+rn = rn[rn['highway'].notnull()]
+print(str(len(rn)), "road sections")
+
+print(datetime.now(), "remove columns")
+rn.drop(columns=['name', 'aerialway', 'waterway', 'barrier', 'man_made', 'z_order'], inplace=True)
+
 
 #attributes:
 #maxspeed
@@ -11,3 +25,12 @@
 #smoothness
 #surface
 #access
+
+
+#TODO: keep only segment ?
+#TODO: compute duration and keep only that ?
+
+#Highway tag
+#https://wiki.openstreetmap.org/wiki/Key:highway
+#motorway, trunk, primary, secondary, tertiary, residential, service, etc.
+
