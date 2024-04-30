d2e4f5f6cd4dcfa8e69fa7a20c95da9ad367cee7
jgaffuri <jgaf@proton.me>
2024-04-30 23:32:29 +0200

    a

--- src/osm/prepare.sh
+++ src/osm/prepare.sh
@@ -1,6 +1,8 @@
 #convert to gpkg and reproject
 ogr2ogr -t_srs EPSG:3035 input.osm europe.gpkg
 
-#extract for road network
+#extract information for road network
 ogr2ogr -sql "SELECT geom,fid,highway,other_tags FROM lines WHERE highway IS NOT NULL" -dialect SQLite -nln lines /home/juju/geodata/OSM/europe_road_network_prep.gpkg /home/juju/geodata/OSM/europe.gpkg
 
+#extract information for buildings
+ogr2ogr -sql "SELECT geom,fid,building,other_tags FROM multipolygons WHERE building IS NOT NULL" -dialect SQLite -nln multipolygons /home/juju/geodata/OSM/europe_road_buildings_prep.gpkg /home/juju/geodata/OSM/europe.gpkg
