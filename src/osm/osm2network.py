3e12d7eb7802650686055400eded88cc359868a0
jgaffuri <jgaf@proton.me>
2024-05-01 09:50:53 +0200

    a

--- src/osm/osm2network.py
+++ src/osm/osm2network.py
@@ -20,19 +20,16 @@ def extract_attributes_from_other_tags(gdf, attributes, delete_other_tags=True):
         gdf[attribute] = None
 
     #iterate through features
-    for iii, feature in gdf.iterrows():
+    for i, feature in gdf.iterrows():
         try:
             #get other tags
             other_tags = feature.other_tags
             if other_tags == None: continue
             #transform it into a dictionnary
             other_tags_dict = other_tags_to_dict(other_tags)
-            #other_tags_dict = {} if other_tags == None else other_tags_to_dict(other_tags)
             #set attribute values
             for attribute in attributes:
-                if not attribute in other_tags_dict: continue #feature[attribute] = None
-                feature[attribute] = other_tags_dict[attribute]
-                #gdf.at[iii, attribute] = other_tags_dict[attribute]
+                if attribute in other_tags_dict: gdf.at[i, attribute] = other_tags_dict[attribute]
         except Exception as e: print("Could not parse other_tags attribute: ", other_tags)
 
     #delete other_tags attribute
