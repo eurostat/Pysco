d72edd5b69f39384a472ae0a8b5de659610f2673
jgaffuri <jgaf@proton.me>
2024-05-02 10:22:02 +0200

    a

--- src/accessibility/a.py
+++ src/accessibility/a.py
@@ -15,42 +15,41 @@ from utils.osmutils import osm_duration
 bbox = [4000000, 2800000, 4100000, 2900000]
 #bbox = [4100000, 2810000, 4110000, 2820000]
 
-partition_size = 10000
+partition_size = 100000
 extention_buffer = 30000
-grid_resolution = 1000
 num_processors_to_use = 8
 cell_id_fun = lambda x,y: "CRS3035RES"+str(grid_resolution)+"mN"+str(int(y))+"E"+str(int(x))
-detailled = True
-
-
 out_folder = "/home/juju/gisco/grid_accessibility_quality/"
 
-#set variables
-
-""""
-#OME2
-case = "OME2"
 pois_loader = lambda bbox: gpd.read_file('/home/juju/geodata/gisco/healthcare_EU_3035.gpkg', bbox=bbox)
-road_network_loader = lambda bbox: gpd.read_file('/home/juju/geodata/OME2_HVLSP_v1/gpkg/ome2.gpkg', layer="tn_road_link", bbox=bbox)
-weight_function = lambda feature, length : ome2_duration(feature, length)
-"""
 
-#OSM
-case = "OSM"
-pois_loader = lambda bbox: gpd.read_file('/home/juju/geodata/gisco/healthcare_EU_3035.gpkg', bbox=bbox)
-road_network_loader = lambda bbox: gpd.read_file('/home/juju/geodata/OSM/europe_road_network_prep.gpkg', bbox=bbox)
-weight_function = lambda feature, length : osm_duration(feature, length)
+for grid_resolution in [100, 1000]:
+    for detailled in [True]:
 
-#execute accessibility analysis
-accessibility_grid(pois_loader,
-                       road_network_loader,
-                       weight_function,
-                       bbox,
-                       out_folder,
-                       "accessibility_grid_" + case + "_" + str(grid_resolution) + "_" + str(detailled),
-                       cell_id_fun=cell_id_fun,
-                       grid_resolution=grid_resolution,
-                       partition_size = partition_size,
-                       extention_buffer = extention_buffer,
-                       detailled = detailled,
-                       num_processors_to_use = num_processors_to_use)
+        print("OME2", grid_resolution, detailled)
+        accessibility_grid(pois_loader,
+                            lambda bbox: gpd.read_file('/home/juju/geodata/OME2_HVLSP_v1/gpkg/ome2.gpkg', layer="tn_road_link", bbox=bbox),
+                            lambda feature, length : ome2_duration(feature, length),
+                            bbox,
+                            out_folder,
+                            "accessibility_grid_OME2_" + str(grid_resolution) + "_" + str(detailled),
+                            cell_id_fun=cell_id_fun,
+                            grid_resolution=grid_resolution,
+                            partition_size = partition_size,
+                            extention_buffer = extention_buffer,
+                            detailled = detailled,
+                            num_processors_to_use = num_processors_to_use)
+
+        print("OSM", grid_resolution, detailled)
+        accessibility_grid(pois_loader,
+                            lambda bbox: gpd.read_file('/home/juju/geodata/OSM/europe_road_network_prep.gpkg', bbox=bbox),
+                            lambda feature, length : osm_duration(feature, length),
+                            bbox,
+                            out_folder,
+                            "accessibility_grid_OSM_" + str(grid_resolution) + "_" + str(detailled),
+                            cell_id_fun=cell_id_fun,
+                            grid_resolution=grid_resolution,
+                            partition_size = partition_size,
+                            extention_buffer = extention_buffer,
+                            detailled = detailled,
+                            num_processors_to_use = num_processors_to_use)
