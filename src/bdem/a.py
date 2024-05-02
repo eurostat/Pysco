ca045b30199aa941d1baf6d6d0ff530ebb2dd3cc
jgaffuri <jgaf@proton.me>
2024-05-02 15:23:56 +0200

    a

--- src/bdem/a.py
+++ src/bdem/a.py
@@ -6,7 +6,6 @@ from building_demography import building_demography_grid
 
 bbox = [3800000, 2700000, 4200000, 3000000]
 #bbox = [4000000, 2800000, 4050000, 2850000]
-resolution = 1000
 partition_size = 100000
 num_processors_to_use = 8
 out_folder = '/home/juju/gisco/building_demography/'
@@ -25,8 +24,8 @@ for case in ["BDTOPO", "OSM"]:
                 lambda bbox: gpd.read_file('/home/juju/geodata/FR/BDTOPO_3-3_TOUSTHEMES_GPKG_LAMB93_R44_2023-12-15/BDT_3-3_GPKG_3035_R44-ED2023-12-15.gpkg', layer='batiment', bbox=bbox),
                 bbox,
                 out_folder,
-                "bu_dem_fr_bdtopo_grid_"+str(resolution),
-                grid_resolution=resolution,
+                "bu_dem_grid_"+case+"_" + str(grid_resolution),
+                grid_resolution = grid_resolution,
                 partition_size = partition_size,
                 nb_floors_fun = lambda f: 1 if f.hauteur==None or isnan(f.hauteur) else ceil(f.hauteur/3.7),
                 residential_fun = lambda f: 1 if f.usage_1=="Résidentiel" else 0.3 if f.usage_2=="Résidentiel" else 0.1 if f.usage_1=="Indifférencié" else 0,
@@ -41,8 +40,8 @@ for case in ["BDTOPO", "OSM"]:
                 lambda bbox: gpd.read_file('/home/juju/geodata/OSM/FR/buildings_grand_est.gpkg', bbox=bbox),
                 bbox,
                 out_folder,
-                "bu_dem_fr_osm_grid_"+str(resolution),
-                grid_resolution=resolution,
+                "bu_dem_grid_"+case+"_" + str(grid_resolution),
+                grid_resolution = grid_resolution,
                 partition_size = partition_size,
                 nb_floors_fun = lambda f: 1,
                 residential_fun = lambda f: 0,
