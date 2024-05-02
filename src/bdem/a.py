38a9836a1e2aec79b7001b2198cf9f94d1f350ce
jgaffuri <jgaf@proton.me>
2024-05-02 15:20:30 +0200

    a

--- src/bdem/a.py
+++ src/bdem/a.py
@@ -11,38 +11,38 @@ partition_size = 100000
 num_processors_to_use = 8
 out_folder = '/home/juju/gisco/building_demography/'
 
-# TODO accessibility. make with OME2 - then OSM, tomtom
-# TODO compare OSM/BDTOPO
-# TODO activity: residential, industrial, agri, services, +activity
+# TODO
+# activity: residential, industrial, agri, services, +activity
 # gridviz - build_dem. for cult heritage
+# date of construction
 
 
-
-# FR bd topo
-
-buildings_loader = lambda bbox: gpd.read_file('/home/juju/geodata/FR/BDTOPO_3-3_TOUSTHEMES_GPKG_LAMB93_R44_2023-12-15/BDT_3-3_GPKG_3035_R44-ED2023-12-15.gpkg', layer='batiment', bbox=bbox)
-nb_floors_fun = lambda f: 1 if f.hauteur==None or isnan(f.hauteur) else ceil(f.hauteur/3.7)
-residential_fun = lambda f: 1 if f.usage_1=="Résidentiel" else 0.3 if f.usage_2=="Résidentiel" else 0.1 if f.usage_1=="Indifférencié" else 0
-cultural_value_fun = lambda f: 1 if f.usage_1=="Religieux" or f.nature=="Tour, donjon" or f.nature=="Monument" or f.nature=="Moulin à vent" or f.nature=="Arc de triomphe" or f.nature=="Fort, blockhaus, casemate" or f.nature=="Eglise" or f.nature=="Château" or f.nature=="Chapelle" or f.nature=="Arène ou théâtre antique" else 0
-out_file = "bu_dem_fr_bdtopo_grid_"+str(resolution)
-
-# FR OSM
-buildings_loader = lambda bbox: gpd.read_file('/home/juju/geodata/OSM/FR/buildings_grand_est.gpkg', bbox=bbox)
-nb_floors_fun = lambda f: 1
-residential_fun = lambda f: 0
-cultural_value_fun = lambda f: 0
-out_file = "bu_dem_fr_osm_grid_"+str(resolution)
-
-
+print("FR BD Topo")
 building_demography_grid(
-    buildings_loader,
+    lambda bbox: gpd.read_file('/home/juju/geodata/FR/BDTOPO_3-3_TOUSTHEMES_GPKG_LAMB93_R44_2023-12-15/BDT_3-3_GPKG_3035_R44-ED2023-12-15.gpkg', layer='batiment', bbox=bbox),
     bbox,
     out_folder,
-    out_file,
+    "bu_dem_fr_bdtopo_grid_"+str(resolution),
     grid_resolution=resolution,
     partition_size = partition_size,
-    nb_floors_fun=nb_floors_fun,
-    residential_fun=residential_fun,
-    cultural_value_fun=cultural_value_fun,
+    nb_floors_fun = lambda f: 1 if f.hauteur==None or isnan(f.hauteur) else ceil(f.hauteur/3.7),
+    residential_fun = lambda f: 1 if f.usage_1=="Résidentiel" else 0.3 if f.usage_2=="Résidentiel" else 0.1 if f.usage_1=="Indifférencié" else 0,
+    cultural_value_fun = lambda f: 1 if f.usage_1=="Religieux" or f.nature=="Tour, donjon" or f.nature=="Monument" or f.nature=="Moulin à vent" or f.nature=="Arc de triomphe" or f.nature=="Fort, blockhaus, casemate" or f.nature=="Eglise" or f.nature=="Château" or f.nature=="Chapelle" or f.nature=="Arène ou théâtre antique" else 0,
+    num_processors_to_use = num_processors_to_use
+) 
+
+
+
+print("OSM")
+building_demography_grid(
+    lambda bbox: gpd.read_file('/home/juju/geodata/OSM/FR/buildings_grand_est.gpkg', bbox=bbox),
+    bbox,
+    out_folder,
+    "bu_dem_fr_osm_grid_"+str(resolution),
+    grid_resolution=resolution,
+    partition_size = partition_size,
+    nb_floors_fun = lambda f: 1,
+    residential_fun = lambda f: 0,
+    cultural_value_fun = lambda f: 0,
     num_processors_to_use = num_processors_to_use
 ) 
