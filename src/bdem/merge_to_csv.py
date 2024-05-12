1d1278bfffa19cea8b8a75796067a9b41b5a3cab
jgaffuri <jgaf@proton.me>
2024-05-12 21:34:48 +0200

    a

--- src/bdem/merge_to_csv.py
+++ src/bdem/merge_to_csv.py
@@ -1,13 +1,17 @@
 import fiona
+import csv
 
 # Path to the GeoPackage file
 gpkg_file = '/home/juju/gisco/building_demography/out_partition/eurobudem_100m_4000000_2500000.gpkg'
+csv_file = '/home/juju/gisco/building_demography/building_demography.csv'
 
 with fiona.open(gpkg_file, 'r') as src:
-    for feature in src:
-        number = feature['properties']['number']
-        if number<=0: continue
-        print(number)
-        #print("geom:", geometry)
-        #print("props:", properties)
+   with open(csv_file, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)        
+        writer.writerow(['property1', 'property2'])
+
+        for feature in src:
+            number = feature['properties']['number']
+            if number<=0: continue
+            writer.writerow(["aaa", number])
 
