7625c496778fa6de3130598ebdefd8d1d5f2ef1a
jgaffuri <jgaf@proton.me>
2024-05-12 19:26:58 +0200

    a

--- src/bdem/merge_to_csv.py
+++ src/bdem/merge_to_csv.py
@@ -5,9 +5,9 @@ gpkg_file = '/home/juju/gisco/building_demography/out_partition/eurobudem_100m_4
 
 with fiona.open(gpkg_file, 'r') as src:
     for feature in src:
-        geometry = feature['geometry']
-        properties = feature['properties']        
-        print("geom:", geometry)
-        print("props:", properties)
+        number = feature['properties']['number']
+        if number<=0: continue
+        print(number)
+        #print("geom:", geometry)
+        #print("props:", properties)
 
-#number = feature['properties']['number']
