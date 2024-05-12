10654a2853c69a06e3d9bab40224ab7f477fa198
jgaffuri <jgaf@proton.me>
2024-05-12 21:51:56 +0200

    a

--- src/bdem/merge_to_csv.py
+++ src/bdem/merge_to_csv.py
@@ -1,18 +1,31 @@
 import fiona
 import csv
+import os
 
-# Path to the GeoPackage file
-gpkg_file = '/home/juju/gisco/building_demography/out_partition/eurobudem_100m_4000000_2500000.gpkg'
 csv_file = '/home/juju/gisco/building_demography/building_demography.csv'
+gpkg_folder = '/home/juju/gisco/building_demography/out_partition/'
+gpkg_files = os.listdir(gpkg_folder)
 
-with fiona.open(gpkg_file, 'r') as src:
-    schema = src.schema
-    fieldnames = [field for field in schema['properties'].keys() if field != 'geometry']
-    with open(csv_file, 'w', newline='') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for feature in src:
-            number = feature['properties']['number']
-            if number<=0: continue
-            properties = {key: value for key, value in feature['properties'].items() if key != 'geometry'}
-            writer.writerow(properties)
+#open CSV file
+with open(csv_file, 'w', newline='') as csvfile:
+
+    for i, gpkg_file in enumerate(gpkg_files):
+        print(gpkg_folder + gpkg_file)
+        with fiona.open(gpkg_folder + gpkg_file, 'r') as src:
+
+            if i==0:
+                #write header
+                schema = src.schema
+                fieldnames = [field for field in schema['properties'].keys() if field != 'geometry']
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                writer.writeheader()
+
+            #write columns
+            for feature in src:
+
+                #skip those with number <=0
+                if feature['properties']['number'] <= 0: continue
+
+                #write properties
+                properties = {key: value for key, value in feature['properties'].items() if key != 'geometry'}
+                writer.writerow(properties)
