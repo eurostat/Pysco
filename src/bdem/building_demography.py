b7983e7521773b9655d51b321e38fc947908d989
jgaffuri <jgaf@proton.me>
2024-05-14 15:12:35 +0200

    a

--- src/bdem/building_demography.py
+++ src/bdem/building_demography.py
@@ -63,7 +63,6 @@ def building_demography_grid(buildings_loader,
 
                 #make grid cell geometry
                 cell_geometry = Polygon([(x, y), (x+grid_resolution, y), (x+grid_resolution, y+grid_resolution), (x, y+grid_resolution)])
-                c["geometry"] = mapping(cell_geometry)
 
                 #get buildings intersecting cell, using spatial index
                 #buildings_ = buildings.sindex.intersection(cell_geometry.bounds)
@@ -96,7 +95,7 @@ def building_demography_grid(buildings_loader,
                     #building number
                     nb = ground_area/bug.area
                     if nb>1: nb=1
-                    tot_nb += nb
+                    p["number"] += nb
 
                     #building area
                     tot_ground_area += ground_area
@@ -119,33 +118,27 @@ def building_demography_grid(buildings_loader,
                     tot_cult_floor_area += cult * floor_area
 
                 #skip empty cells
-                if skip_empty_cells and tot_nb == 0: continue
+                if skip_empty_cells and p["number"] == 0: continue
 
                 #round values
-                tot_nb = round(tot_nb, 2)
-                tot_ground_area = round(tot_ground_area)
-                tot_floor_area = round(tot_floor_area)
-                tot_res_ground_area = round(tot_res_ground_area)
-                tot_res_floor_area = round(tot_res_floor_area)
-                tot_activity_ground_area = round(tot_activity_ground_area)
-                tot_activity_floor_area = round(tot_activity_floor_area)
-                tot_cult_ground_area = round(tot_cult_ground_area)
-                tot_cult_floor_area = round(tot_cult_floor_area)
+                p["number"] = round(p["number"], 2)
+                p["ground_area"] = round(p["ground_area"])
+                p["floor_area"] = round(p["floor_area"])
+                p["residential_ground_area"] = round(p["residential_ground_area"])
+                p["residential_floor_area"] = round(p["residential_floor_area"])
+                p["economic_activity_ground_area"] = round(p["economic_activity_ground_area"])
+                p["economic_activity_floor_area"] = round(p["economic_activity_floor_area"])
+                p["cultural_ground_area"]  = round(p["cultural_ground_area"])
+                p["cultural_floor_area"] = round(p["cultural_floor_area"])
 
-                #store cell values
-                cell_geometries.append(cell_geometry)
-                tot_nbs.append(tot_nb)
-                tot_ground_areas.append(tot_ground_area)
-                tot_floor_areas.append(tot_floor_area)
-                tot_res_ground_areas.append(tot_res_ground_area)
-                tot_res_floor_areas.append(tot_res_floor_area)
-                tot_activity_ground_areas.append(tot_activity_ground_area)
-                tot_activity_floor_areas.append(tot_activity_floor_area)
-                tot_cult_ground_areas.append(tot_cult_ground_area)
-                tot_cult_floor_areas.append(tot_cult_floor_area)
-
-                #store cell code
+                #cell code
                 p["GRD_ID"] = cell_id_fun(x,y)
+
+                #cell geometry
+                c["geometry"] = mapping(cell_geometry)
+
+                #add new cell
+                cells.append(c)
 
         return cells
 
@@ -167,10 +160,8 @@ def building_demography_grid(buildings_loader,
             return
 
         print(datetime.now(), "save as GPKG")
-
         schema = get_schema_from_feature(cells[0])
         dst = fiona.open(out_folder+out_file+".gpkg", 'w', driver='GPKG', crs=CRS.from_epsg(crs), schema=schema)
         dst.writerecords(cells)
-        #for f in cells: dst.write(f)
 
         print(datetime.now(), "Done")
