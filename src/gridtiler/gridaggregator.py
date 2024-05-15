6ab30d40a0f7b4ead63d317eec698d9efc9cc99a
jgaffuri <jgaf@proton.me>
2024-05-16 00:50:10 +0200

    a

--- src/gridtiler/gridaggregator.py
+++ src/gridtiler/gridaggregator.py
@@ -13,12 +13,14 @@ def grid_aggregation(
     # the aggregated cells, indexed by xa and then ya
     aggregation_index = {}
     target_resolution = a*resolution
+    keys = None
 
     with open(input_file, 'r') as csvfile:
         csvreader = csv.DictReader(csvfile)
 
         #iterate through cells from the input CSV file
         for c in csvreader:
+            if keys==None: keys = list(c.keys())
 
             #get aggregated cell x,y
             xa = target_resolution * floor(c["x"] / target_resolution)
@@ -35,10 +37,35 @@ def grid_aggregation(
                 cA_[ya] = cA
             cA.append(c)
 
-            #aggregation function
-            #TODO handle other cases: average, mode, etc
-            aggregation_fun = sum
+    #aggregation function
+    #TODO handle other cases: average, mode, etc
+    aggregation_fun = sum
 
-            #prepare function to round aggregated figures
-            tolerance = pow(10, aggregation_rounding)
-            roundToTolerance = lambda number : round(number * tolerance) / tolerance
+    #prepare function to round aggregated figures
+    tolerance = pow(10, aggregation_rounding)
+    roundToTolerance = lambda number : round(number * tolerance) / tolerance
+
+    #aggregate cell values
+    for xa, d in aggregation_index.items():
+        for ya, cells in d.items():
+
+            print(xa,ya,len(cells))
+
+            #TODO compute aggregated values
+            #TODO write in file
+
+            #release memory immediatelly
+            del d[ya]
+
+            """
+            //compute aggregates values
+            for (let k of keys) {
+                //get list of values to aggregate
+                const vs = []
+                for (let c of cA.cells) vs.push(c[k])
+                //compute and set aggregated value
+                cA[k] = aggregateSum(vs)
+                if (opts.aggregationRounding != undefined)
+                    cA[k] = roundToTolerance(cA[k])
+            }
+            """    
