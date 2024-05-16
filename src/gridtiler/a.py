94c65f1b33e064b8f4b312a9b37f034cdab6a69c
jgaffuri <jgaf@proton.me>
2024-05-16 08:51:32 +0200

    a

--- src/gridtiler/a.py
+++ src/gridtiler/a.py
@@ -1,5 +1,19 @@
 from gridtiler import grid_tiling
+from gridaggregator import grid_aggregation
 
+
+
+grid_aggregation(
+    "/home/juju/gisco/building_demography/building_demography.csv",
+    100,
+    '/home/juju/Bureau/out_1000.csv',
+    10
+)
+
+
+
+
+""""
 
 #set cell x,y from its grid_id
 def position_fun(c):
@@ -19,3 +33,4 @@ grid_tiling(
     crs = "EPSG:3035",
     preprocess = position_fun
 )
+"""
