7204e7006d1d49253b154bce3b96f5aedcdb634e
jgaffuri <jgaf@proton.me>
2024-05-22 22:16:34 +0200

    a

--- src/bdem/population_disaggregation.py
+++ src/bdem/population_disaggregation.py
@@ -9,3 +9,12 @@ cnt = ["FR", "NL", "PL", "IT", "LU"]
 #load 100m budem cells in bbox
 #load 1000m population cells in bbox
 
+#index 100m cells
+
+#for each 1000m population cell with non-null population
+
+    #get 100m cells
+    #compute total bu_res
+    #for each 100m cell
+        #assign 100m population as pop*bu_res/tot_bu_res
+
