3b23e4ecbf86bc9fc8ea366dca94c116ba384e96
jgaffuri <jgaf@proton.me>
2024-06-17 11:09:34 +0200

    a

--- src/census2021/to_gridviz.py
+++ src/census2021/to_gridviz.py
@@ -1,9 +1,10 @@
 from pygridmap import gridtiler
 from datetime import datetime
-
+import os
 
 transform = False
-aggregation = True
+aggregation = False
+tiling = True
 
 folder = "/home/juju/geodata/census/"
 input_file = folder + "csv_export.csv"
@@ -43,3 +44,24 @@ if aggregation:
         print(datetime.now(), "aggregation to", a*10000, "m")
         gridtiler.grid_aggregation(input_file=folder+"out/10000.csv", resolution=10000, output_file=folder+"out/"+str(a*10000)+".csv", a=a)
 
+
+
+#tiling
+if tiling:
+    for resolution in [1000, 2000, 5000, 10000, 20000, 50000, 100000]:
+        print("tiling for resolution", resolution)
+
+        #create output folder
+        out_folder = folder + 'out/' + str(resolution)
+        if not os.path.exists(folder): os.makedirs(folder)
+
+        gridtiler.grid_tiling(
+            folder+"out/"+str(resolution)+'.csv',
+            out_folder,
+            resolution,
+            tile_size_cell = 256,
+            x_origin = 0,
+            y_origin = 0,
+            crs = "EPSG:3035",
+            format = "parquet"
+        )
