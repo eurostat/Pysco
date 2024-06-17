029219423a5d9beb7af6c7e31bdc63c665a604c3
jgaffuri <jgaf@proton.me>
2024-06-17 10:32:23 +0200

    a

--- src/census2021/parquet_to_gridviz.py
+++ src/census2021/parquet_to_gridviz.py
@@ -1,6 +1,19 @@
+from pygridmap import gridtiler
+
+
+folder = "/home/juju/geodata/census/"
+input_file = folder + "csv_export.csv"
+
+
+def tr(c):
+    print(c)
+    #fid,GRD_ID,T,M,F,Y_LT15,Y_1564,Y_GE65,EMP,NAT,EU_OTH,OTH,SAME,CHG_IN,CHG_OUT
+    del c["fid"]
+    for p in "T","M","F","Y_LT15","Y_1564","Y_GE65","EMP","NAT","EU_OTH","OTH,SAME","CHG_IN","CHG_OUT":
+        v = c[p]
+        if v == "0": c[p] = ""
 
 
 
-
-input_file = "/home/juju/geodata/census/compiled.csv"
+gridtiler.grid_transformation(input_file=input_file, output_file=folder+"out/1.csv", function=tr)
 
