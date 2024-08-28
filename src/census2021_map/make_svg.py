5f29640ee024b508c49a259a8d19371b431776d3
jgaffuri <jgaf@proton.me>
2024-08-28 11:02:26 +0200

    a

--- src/census2021_map/make_svg.py
+++ src/census2021_map/make_svg.py
@@ -17,23 +17,14 @@ max_diameter = res * 1.6
 #print(min_diameter, max_diameter)
 power = 0.25
 
-col0, col1, col2 = "#4daf4a", "#377eb8", "#e41a1c"
-colm0, colm1, colm2 = "#ab606a", "#ae7f30", "#4f9685"
-centerColor = "#999"
 
-
-
-c0, c1, c2 = 0.15, 0.6, 0.25
-centerCoefficient = 0.25
-cc = 1-centerCoefficient
-withMixedClasses = True
-
-#TODO
 classifier = trivariate_classifier(
     ['Y_LT15', 'Y_1564', 'Y_GE65'],
     lambda cell:cell["T_"],
     {'center': [0.15, 0.6, 0.25], 'centerCoefficient': 0.25}
     )
+
+colors = {"0": "#4daf4a", "1": "#377eb8", "2": "#e41a1c", "m0": "#ab606a", "m1": "#ae7f30", "m2": "#4f9685", "center": "#999"}
 
 
 def make_map(path_svg = '/home/juju/gisco/census_2021_map/map_age_EUR.svg',
@@ -114,84 +105,11 @@ def make_map(path_svg = '/home/juju/gisco/census_2021_map/map_age_EUR.svg',
         t = pow(t, power)
         diameter = min_diameter + t * (max_diameter - min_diameter)
 
-
-
-
-        p0 = cell['Y_LT15']
-        p1 = cell['Y_1564']
-        p2 = cell['Y_GE65']
-        t = cell['T']
-        tot = p0 + p1 + p2
-
-        if tot == 0: color = "gray"
-        else:
-            #compute shares
-            s0, s1, s2 = p0 / tot, p1 / tot, p2 / tot
-
-            #class 0
-            if s0 >= c0 and s1 <= c1 and s2 <= c2:
-                #central class near class 0
-                if cc != None and (s2 - c2) * (c1 - cc * c1) >= (s1 - cc * c1) * (cc * c2 - c2):
-                    color = centerColor
-                else:
-                    color = col0
-
-            #class 1
-            elif s0 <= c0 and s1 >= c1 and s2 <= c2:
-                #central class near class 1
-                if cc != None and (s2 - c2) * (c0 - cc * c0) >= (s0 - cc * c0) * (cc * c2 - c2):
-                    color = centerColor
-                else:
-                    color = col1
-            
-            #class 2
-            elif s0 <= c0 and s1 <= c1 and s2 >= c2:
-                #central class near class 2
-                if cc != None and (s1 - c1) * (c0 - cc * c0) >= (s0 - cc * c0) * (cc * c1 - c1):
-                    color = centerColor
-                else:
-                    color = col2
-            
-            #middle class 0 - intersection class 1 and 2
-            elif (s0 <= c0 and s1 >= c1 and s2 >= c2):
-                #central class
-                if cc != None and s0 > (cc) * c0: color = centerColor
-                else:
-                    if withMixedClasses: color=colm0
-                    else: color = col1 if s1>s2 else col2
-            
-            #middle class 1 - intersection class 0 and 2
-            elif (s0 >= c0 and s1 <= c1 and s2 >= c2):
-                #central class
-                if cc != None and s1 > (cc) * c1: color = centerColor
-                else:
-                    if withMixedClasses: color=colm1
-                    else: color = col0 if s0>s2 else col2
-            
-            #middle class 2 - intersection class 0 and 1
-            elif (s0 >= c0 and s1 >= c1 and s2 <= c2):
-                #central class
-                if cc != None and s2 > (cc) * c2: color = centerColor
-                else:
-                    if withMixedClasses: color=colm2
-                    else: color = col1 if s1>s0 else col0
-
-            else:
-                print("aaa")
-                color = "blue"
-
-
-        #get color class
+        #get color
         cl = classifier(cell)
-        if cl=="0": color = col0
-        elif cl=="1": color = col1
-        elif cl=="2": color = col2
-        elif cl=="m0": color = colm0
-        elif cl=="m1": color = colm1
-        elif cl=="m2": color = colm2
-        elif cl=="center": color = centerColor
-        else: print("Unexpected class", cl); color="red"
+        color = colors[cl] if cl is not None else "black"
 
+        #draw circle
         gCircles.add(dwg.circle(center=(round(cell['x']+res/2), round(y_min + y_max - cell['y']-res/2)), r=round(diameter/2), fill=color))
 
 
