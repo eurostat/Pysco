6bd45f9c878a5fe296ecc62f32be51cc19de3290
jgaffuri <jgaf@proton.me>
2025-03-18 14:15:03 +0100

    a

--- src/road_performance_indic/a.py
+++ src/road_performance_indic/a.py
@@ -1,4 +1,16 @@
 
 
-print("aaa")
+
+
+tomtom = "/home/juju/geodata/tomtom/tomtom_202312.gpkg"
+population_grid = "/home/juju/geodata/census/2021/ESTAT_Census_2021_V2.gpkg"
+
+
+
+
+def process(tomtom, population_grid):
+    print("Process", tomtom, population_grid)
+
+
+process(tomtom, population_grid)
 
