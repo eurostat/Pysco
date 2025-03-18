d441bee4287db18e89e49e7a2dd22b695f4c379e
jgaffuri <jgaf@proton.me>
2025-03-18 15:03:46 +0100

    a

--- src/road_performance_indic/a.py
+++ src/road_performance_indic/a.py
@@ -1,6 +1,11 @@
-
+from shapely.geometry import box,Polygon
 import geopandas as gpd
-
+from datetime import datetime
+import networkx as nx
+import concurrent.futures
+import fiona
+import fiona.transform
+from shapely.geometry import shape, mapping
 
 
 tomtom = "/home/juju/geodata/tomtom/tomtom_202312.gpkg"
@@ -12,10 +17,18 @@ tomtom_loader = lambda bbox: gpd.read_file('/home/juju/geodata/tomtom/2021/nw.gp
 
 # population straight. Load population grid. Turn into points. Make spatial index.
 def process1(population_grid):
-    print("Process", tomtom, population_grid)
 
-    cells = gpd.read_file(population_grid),
-    print(len(cells), "cells loaded")
+
+    print(datetime.now(), "Loading population grid...", population_grid)
+    cells = fiona.open(population_grid, layer="census2021")
+    print(datetime.now(), len(cells), "cells loaded")
+
+    for c in cells:
+        geom = shape(c["geometry"])
+        centroid = geom.centroid
+        c["geometry"] = mapping(centroid)
+
+
 
 
 process1(population_grid)
