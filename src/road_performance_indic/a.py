ffee4cd055dd4bbc904c5eef1e96fde4e8d4e11f
jgaffuri <jgaf@proton.me>
2025-03-18 14:53:19 +0100

    a

--- src/road_performance_indic/a.py
+++ src/road_performance_indic/a.py
@@ -10,12 +10,14 @@ population_grid = "/home/juju/geodata/census/2021/ESTAT_Census_2021_V2.gpkg"
 tomtom_loader = lambda bbox: gpd.read_file('/home/juju/geodata/tomtom/2021/nw.gpkg', bbox=bbox),
 
 
-
-def process(tomtom, population_grid):
+# population straight. Load population grid. Turn into points. Make spatial index.
+def process1(population_grid):
     print("Process", tomtom, population_grid)
 
+    cells = gpd.read_file(population_grid),
+    print(len(cells), "cells loaded")
 
-process(tomtom, population_grid)
 
+process1(population_grid)
 
 
