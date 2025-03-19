02bdcba11a1021e0fc46026905b1b316725234f6
jgaffuri <jgaf@proton.me>
2025-03-19 11:14:31 +0100

    a

--- src/road_performance_indic/a.py
+++ src/road_performance_indic/a.py
@@ -2,8 +2,8 @@ from shapely.geometry import box,Polygon
 import geopandas as gpd
 from datetime import datetime
 import networkx as nx
-import concurrent.futures
-import threading
+#import concurrent.futures
+#import threading
 import fiona
 import fiona.transform
 from shapely.geometry import shape, mapping
@@ -39,15 +39,17 @@ def compute_nearby_population(population_grid, layer, nearby_population_csv, onl
 
     print(datetime.now(), "index cells...")
     spatial_index = index.Index()
-    cells_ = {}
+    cells_ = []
 
     # feed index
-    for i, c in enumerate(cells):
+    i = 0
+    for c in cells:
         c = c[1]
         geom = shape(c["geometry"])
         pt = geom.centroid
         spatial_index.insert(i, geom.bounds)
-        cells_[i] = {"pop":c["properties"]["T"], "x":pt.x, "y":pt.y, "GRD_ID": c["properties"]["GRD_ID"]}
+        cells_.append( {"pop":c["properties"]["T"], "x":pt.x, "y":pt.y, "GRD_ID": c["properties"]["GRD_ID"]} )
+        i += 1
 
     #free memory
     del cells
@@ -64,8 +66,8 @@ def compute_nearby_population(population_grid, layer, nearby_population_csv, onl
 
         #get close cells using spatial index
         close_cells = None
-        with lock:
-            close_cells = list(spatial_index.intersection((x-radius_m, y-radius_m, x+radius_m, y+radius_m)))
+        #with lock:
+        close_cells = list(spatial_index.intersection((x-radius_m, y-radius_m, x+radius_m, y+radius_m)))
 
         #compute population total
         pop_tot = 0
@@ -83,12 +85,12 @@ def compute_nearby_population(population_grid, layer, nearby_population_csv, onl
         return { "pop":pop_tot, "GRD_ID":c["GRD_ID"] }
 
     #compute, not in parallel
-    #output = []
-    #for c in cells_.values(): output.append(compute_cell_indicator(c))
+    output = []
+    for c in cells_: output.append(compute_cell_indicator(c))
 
     # Run in parallel
-    executor = concurrent.futures.ProcessPoolExecutor()
-    output = list(executor.map(compute_cell_indicator, cells_.values()))
+    #executor = concurrent.futures.ThreadPoolExecutor()
+    #output = list(executor.map(compute_cell_indicator, cells_))
 
     print(datetime.now(), "Free memory")
     del spatial_index
