ff106fe7997b55553f319e5bcca6e4aad984a4f5
jgaffuri <jgaf@proton.me>
2025-04-03 10:55:22 +0200

    a

--- src/census2021/census2021gridvalidation.py
+++ src/census2021/census2021gridvalidation.py
@@ -8,19 +8,17 @@ from utils.geomutils import average_z_coordinate
 from utils.csvutils import save_as_csv
 
 
-bbox = None #[4200000, 2700000, 4560000, 3450000] #LU
 grid_path = "/home/juju/geodata/census/2021/ESTAT_Census_2021_V2.gpkg"
+bbox = [4200000, 2700000, 4560000, 3450000] #LU
 output_folder = "/home/juju/gisco/census_2021_validation/"
-os.makedirs(output_folder, exist_ok=True)
 
+#prepare output
+os.makedirs(output_folder, exist_ok=True)
 errors = []
 
 print("Load grid cells from", grid_path)
 cells = loadFeatures(grid_path, bbox)
 print(len(cells), "cells loaded")
-
-
-
 
 
 #(['geometry', 'GRD_ID', 'T',
@@ -45,12 +43,17 @@ for c in cells:
 
     #check CI_XXX values
     for att in [ 'T_CI', 'M_CI', 'F_CI', 'Y_LT15_CI', 'Y_1564_CI', 'Y_GE65_CI', 'EMP_CI', 'NAT_CI', 'EU_OTH_CI', 'OTH_CI', 'SAME_CI', 'CHG_IN_CI', 'CHG_OUT_CI']:
-        v = c[att]
+        ci = c[att]
         if v==None or v==-9999: continue
         err_codes.append(att+"_value="+str(v))
 
     #check consitency CI_XXX and XXX
-    #TODO
+    for att in ['T','M', 'F', 'Y_LT15', 'Y_1564', 'Y_GE65', 'EMP', 'NAT', 'EU_OTH', 'OTH', 'SAME', 'CHG_IN', 'CHG_OUT']:
+        v = c[att]
+        ci = c[att+"_CI"]
+        if ci==-9999 and v==-9999: continue
+        if ci==None and v>=0: continue
+        err_codes.append(att+"_CI_inconsistency_ci="+str(ci)+"_value="+str(v))
 
     #check POPULATED values
     att = 'POPULATED'
@@ -77,12 +80,48 @@ for c in cells:
 
 
 
+
+
+
+#TODO move
+def csv_grid_to_geopackage(csv_grid_path, gpkg_grid_path, geom="surf"):
+
+    #load csv
+    data = None
+    with open(csv_grid_path, mode="r", newline="") as file:
+        reader = csv.DictReader(file)
+        data = list(reader)
+
+    for row in data:
+        print(row)
+
+        #make cell
+        c = {"type":"Feature", "properties":{}}
+        p = c["properties"]
+
+        #make grid cell geometry
+        cell_geometry = Polygon([(x, y), (x+grid_resolution, y), (x+grid_resolution, y+grid_resolution), (x, y+grid_resolution)])
+        #cell geometry
+        c["geometry"] = mapping(cell_geometry)
+
+    schema = get_schema_from_feature(cells[0])
+    out = fiona.open(out_folder+out_file+".gpkg", 'w', driver='GPKG', crs=CRS.from_epsg(crs), schema=schema)
+    out.writerecords(cells)
+
+
+
+
+
+
 print(len(errors), "errors found")
 
 if len(errors)>0:
     #sort by error
     errors = sorted(errors, key=lambda x: x["errors"])
-    out_filename = output_folder + "errors.csv"
-    print("Save to ", out_filename)
-    save_as_csv(out_filename, errors)
+    print("Save to ", output_folder + "errors.csv")
+    save_as_csv(output_folder + "errors.csv", errors)
+
+    print("Save to ", output_folder + "errors.gpkg")
+    csv_grid_to_geopackage(output_folder + "errors.csv", output_folder + "errors.gpkg")
+
 
