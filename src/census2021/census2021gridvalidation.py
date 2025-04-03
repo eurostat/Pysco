dbe9ef7cf391a7e9fa074858af2a030fd723e810
jgaffuri <jgaf@proton.me>
2025-04-03 21:02:09 +0200

    a

--- src/census2021/census2021gridvalidation.py
+++ src/census2021/census2021gridvalidation.py
@@ -36,7 +36,6 @@ print(len(cells), "cells loaded")
 print("Run validation cell by cell...")
 
 
-
 #function to check the categories sum up to the total population
 def check_categrories_total(cell, categories, categories_label, err_codes):
     t = cell["T"]
@@ -74,7 +73,9 @@ def validation(cells, rules, file_name):
 
         err_codes = []
 
-        #check CI_XXX values: should be either None or -9999
+        #TODO data items on total population shall not be reported as confidential;
+
+        #check CI_XXX values: should be either None (non confidential) or -9999 (confidencial)
         if "ci_val" in rules:
             for att in [ 'T_CI', 'M_CI', 'F_CI', 'Y_LT15_CI', 'Y_1564_CI', 'Y_GE65_CI', 'EMP_CI', 'NAT_CI', 'EU_OTH_CI', 'OTH_CI', 'SAME_CI', 'CHG_IN_CI', 'CHG_OUT_CI']:
                 ci = c[att]
@@ -87,8 +88,11 @@ def validation(cells, rules, file_name):
             for att in ['T','M', 'F', 'Y_LT15', 'Y_1564', 'Y_GE65', 'EMP', 'NAT', 'EU_OTH', 'OTH', 'SAME', 'CHG_IN', 'CHG_OUT']:
                 v = c[att]
                 ci = c[att+"_CI"]
+                #confidencial
                 if ci==-9999 and v==-9999: continue
-                if ci==None and v !=None and v>=0: continue
+                #non confidential
+                if ci==None and v!=-9999: continue
+                #EMP special case, for FR and DE
                 if att=="EMP" and ci==None and v==None: continue
                 err_codes.append(att+"_CI_inconsistency_ci="+str(ci)+"_value="+str(v))
 
@@ -99,10 +103,11 @@ def validation(cells, rules, file_name):
 
         #check consitency POPULATED and T
         if "populated_consis" in rules:
-            att = 'POPULATED'
-            v = c['POPULATED']
-            if v==1 and t<=0: err_codes.append("POPULATED_T_inconsistency_POPULATED="+str(v)+"_T="+str(t))
-            if v==0 and t>0: err_codes.append("POPULATED_T_inconsistency_POPULATED="+str(v)+"_T="+str(t))
+            p = c['POPULATED']
+            # data items on total population with an observed value other than ‘0’ shall be marked with the flag ‘populated’
+            if p==1 and t==0: err_codes.append("POPULATED_T_inconsistency_POPULATED="+str(p)+"_T="+str(t))
+            # data items on total population with an observed value ‘0’ shall not be marked with the flag ‘populated’.
+            if p==0 and t!=0: err_codes.append("POPULATED_T_inconsistency_POPULATED="+str(p)+"_T="+str(t))
 
         #check valid population values: not none
         if "pop_values_none" in rules:
