9748dfba80356a606ee9978f04db9c8e3a94d516
jgaffuri <jgaf@proton.me>
2025-05-13 12:09:29 +0200

    a

--- src/accessibility/knd_test.py
+++ src/accessibility/knd_test.py
@@ -1,15 +1,19 @@
-import geopandas as gpd
+#import geopandas as gpd
 from k_nearest_dijkstra import build_graph_from_gpkg
+from datetime import datetime
 
 
 
 tomtom = "/home/juju/geodata/tomtom/tomtom_202312.gpkg"
 bbox = (4033580, 2945950, 4053279, 2958000)
 
+#data = gpd.read_file(tomtom, bbox=bbox)
+#print(len(data))
 
-data = gpd.read_file(tomtom, bbox=bbox)
-print(len(data))
 
+print(datetime.now(), "make graph")
+graph = build_graph_from_gpkg(tomtom, "nw", bbox)
+#print(graph)
 
-build_graph_from_gpkg()
+print(datetime.now(), "Done")
 
