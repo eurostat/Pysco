d8b76ce68a6a5a87ca1a616395d6151650b45a9c
jgaffuri <jgaf@proton.me>
2025-05-13 11:19:42 +0200

    a

--- src/accessibility/knd_test.py
+++ src/accessibility/knd_test.py
@@ -1,2 +1,11 @@
+import geopandas as gpd
+
+
+tomtom = "/home/juju/geodata/tomtom/tomtom_202312.gpkg"
+bbox = (4033580, 2945950, 4053279, 2958000)
+
+
+data = gpd.read_file(tomtom, bbox=bbox)
+print(len(data))
 
 
