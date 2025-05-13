24ada970142a063725d3813bbcc8d79ca5a10889
jgaffuri <jgaf@proton.me>
2025-05-13 14:39:52 +0200

    a

--- src/accessibility/knd_test.py
+++ src/accessibility/knd_test.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 
 tomtom = "/home/juju/geodata/tomtom/tomtom_202312.gpkg"
-bbox = (4033580, 2945950, 4053279, 2958000)
+bbox = (4034000, 2946000, 4053000, 2958000)
 
 #data = gpd.read_file(tomtom, bbox=bbox)
 #print(len(data))
@@ -15,7 +15,10 @@ print(datetime.now(), "make graph")
 graph = build_graph_from_gpkg(tomtom, "nw", bbox)
 #print(graph)
 
+nodids = graph.keys()
+print(nodids)
 
+exit()
 
 print(datetime.now(), "compute accessiblity")
 out = multi_source_k_nearest_dijkstra(graph=graph, k=3, sources=sources, with_paths=False)
