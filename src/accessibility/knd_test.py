36c9a48185566f5f30d9e244491de2e3c0a3a05e
jgaffuri <jgaf@proton.me>
2025-05-14 18:20:48 +0200

    a

--- src/accessibility/knd_test.py
+++ src/accessibility/knd_test.py
@@ -1,5 +1,6 @@
 import geopandas as gpd
-from accessibility.accessiblity_grid_k_nearest_dijkstra import graph_adjacency_list_from_geodataframe, multi_source_k_nearest_dijkstra, export_dijkstra_results_to_gpkg
+from shapely.geometry import box
+from accessiblity_grid_k_nearest_dijkstra import graph_adjacency_list_from_geodataframe, multi_source_k_nearest_dijkstra, export_dijkstra_results_to_gpkg
 from datetime import datetime
 import random
 
@@ -7,18 +8,29 @@ k=3
 with_paths = False
 nb_sources = 10
 
-tomtom = "/home/juju/geodata/tomtom/tomtom_202312.gpkg"
-bbox = (4034000, 2946000, 4053000, 2958000)
-#bbox = (4000000, 2500000, 4500000, 3000000)
+pois_loader = lambda bbox: gpd.read_file('/home/juju/geodata/gisco/basic_services/healthcare_2023_3035.gpkg', bbox=bbox)
+road_network_loader = lambda bbox: gpd.read_file("/home/juju/geodata/tomtom/tomtom_202312.gpkg", bbox=bbox),
+weight_function = lambda feature, length : -1 if feature.KPH==0 else 1.1*length/feature.KPH*3.6,
+
+bbox = box(4034000, 2946000, 4053000, 2958000)
+#extended_bbox = (4000000, 2500000, 4500000, 3000000)
 
 #data = gpd.read_file(tomtom, bbox=bbox)
 #print(len(data))
 
+print(datetime.now(),"load POIs")
+pois = pois_loader(bbox)
+print(len(pois))
+
 print(datetime.now(), "load road sections")
-roads = gpd.read_file(tomtom, layer="nw", bbox=bbox)
+roads = road_network_loader(bbox)
+print(len(roads), "sections loaded")
+
 print(datetime.now(), "make graph")
 graph = graph_adjacency_list_from_geodataframe(roads)
 del roads
+print(len(graph.keys()), "nodes")
+
 
 print(datetime.now(), "select sources, as random nodes")
 nodids = list(graph.keys())
