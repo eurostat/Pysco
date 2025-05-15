689e4af7ed2cdd3eae9b67a5779e6fbbb08ca3cb
jgaffuri <jgaf@proton.me>
2025-05-15 15:04:44 +0200

    a

--- src/accessibility/knd_test.py
+++ src/accessibility/knd_test.py
@@ -32,8 +32,11 @@ cell_network_max_distance = -1
 
 [x_part,y_part] = [4036000, 2948000]
 #extended_bbox = (4000000, 2500000, 4500000, 3000000)
-partition_size = 10000
-extention_buffer = 2000
+#partition_size = 10000
+#extention_buffer = 2000
+partition_size = 100000
+extention_buffer = 50000
+
 
 #partition extended bbox
 extended_bbox = box(x_part-extention_buffer, y_part-extention_buffer, x_part+partition_size+extention_buffer, y_part+partition_size+extention_buffer)
