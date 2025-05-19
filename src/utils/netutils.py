ee676b122f02259e4b65f785c0695f16b7e77495
jgaffuri <jgaf@proton.me>
2025-05-19 20:30:09 +0200

    a

--- src/utils/netutils.py
+++ src/utils/netutils.py
@@ -3,9 +3,11 @@ from shapely.geometry import LineString
 import math
 from rtree import index
 
+'''
 def shortest_path_geometry(sp):
     coordinates_tuples = [tuple(map(float, coord.split('_'))) for coord in sp]
     return LineString(coordinates_tuples)
+'''
 
 def node_coordinate(node):
     c = node.split('_')
