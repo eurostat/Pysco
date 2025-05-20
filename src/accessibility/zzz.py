dffa0214a5abd7e965312281dba8c794e82d305e
jgaffuri <jgaf@proton.me>
2025-05-20 09:15:01 +0200

    a

--- src/accessibility/zzz.py
+++ src/accessibility/zzz.py
@@ -3,9 +3,9 @@ from accessiblity_grid_k_nearest_dijkstra import accessiblity_grid_k_nearest_dij
 
  
 #luxembourg
-bbox = [4030000, 2940000, 4050000, 2960000]
+#bbox = [4030000, 2940000, 4050000, 2960000]
 #big
-#bbox = [3500000, 2000000, 4000000, 2500000]
+bbox = [3500000, 2000000, 4000000, 2500000]
 
 #test
 #bbox = [3800000, 2300000, 4000000, 2500000]
@@ -42,8 +42,8 @@ accessiblity_grid_k_nearest_dijkstra(
     cell_id_fun = lambda x,y: "CRS3035RES"+str(grid_resolution)+"mN"+str(int(y))+"E"+str(int(x)),
     grid_resolution= grid_resolution,
     cell_network_max_distance= grid_resolution * 1.5,
-    partition_size = 10000,
-    extention_buffer = 1000,
+    partition_size = 100000,
+    extention_buffer = 30000,
     detailled = True,
     duration_simplification_fun = lambda x:round(x,1),
     crs = 'EPSG:3035',
