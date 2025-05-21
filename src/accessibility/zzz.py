975abbd4b1450ef75a6d6ef4a3ef1ec8a99bdf70
jgaffuri <jgaf@proton.me>
2025-05-21 09:54:00 +0200

    a

--- src/accessibility/zzz.py
+++ src/accessibility/zzz.py
@@ -3,9 +3,9 @@ from accessiblity_grid_k_nearest_dijkstra import accessiblity_grid_k_nearest_dij
 
 
 #luxembourg
-#bbox = [4030000, 2940000, 4050000, 2960000]
+bbox = [4030000, 2940000, 4050000, 2960000]
 #big
-bbox = [3500000, 2000000, 4000000, 2500000]
+#bbox = [3500000, 2000000, 4000000, 2500000]
 
 #test
 #bbox = [3800000, 2300000, 4000000, 2500000]
@@ -25,6 +25,36 @@ def direction_fun(feature):
 #TODO fix parallelism
 #TODO improve data loading: use fiona to load only necessary features, with the where ?
 #TODO handle case when speed depends on driving direction
+
+
+
+accessiblity_grid_k_nearest_dijkstra(
+    pois_loader = lambda bbox: gpd.read_file('/home/juju/geodata/gisco/basic_services/healthcare_2023_3035.gpkg', bbox=bbox),
+    road_network_loader = lambda bbox: gpd.read_file('/home/juju/geodata/tomtom/tomtom_202312.gpkg', bbox=bbox).query("ONEWAY != 'N'"),
+    bbox = bbox,
+    out_folder = "/home/juju/Bureau/",
+    out_file = "grid",
+    k = 3,
+    weight_function = lambda feature, length : -1 if feature.KPH==0 else 1.1*length/feature.KPH*3.6,
+    direction_fun = direction_fun,
+    is_not_snappable_fun = lambda f: f.RAMP==1 or f.FOW==5 or (f.FOW==1 and f.FRC==0),
+    initial_node_level_fun = lambda f:f.F_ELEV,
+    final_node_level_fun = lambda f:f.T_ELEV,
+    cell_id_fun = lambda x,y: "CRS3035RES"+str(grid_resolution)+"mN"+str(int(y))+"E"+str(int(x)),
+    grid_resolution= grid_resolution,
+    cell_network_max_distance= grid_resolution * 1.5,
+    partition_size = 5000,
+    extention_buffer = 5000,
+    detailled = True,
+    duration_simplification_fun = lambda x:round(x,1),
+    crs = 'EPSG:3035',
+    num_processors_to_use = 1,
+    save_GPKG = True,
+    save_CSV = False,
+    save_parquet = False
+)
+
+
 
 '''
 accessiblity_grid_k_nearest_dijkstra(
@@ -56,29 +86,3 @@ accessiblity_grid_k_nearest_dijkstra(
 
 
 
-
-accessiblity_grid_k_nearest_dijkstra(
-    pois_loader = lambda bbox: gpd.read_file('/home/juju/geodata/gisco/basic_services/healthcare_2023_3035.gpkg', bbox=bbox),
-    road_network_loader = lambda bbox: gpd.read_file('/home/juju/geodata/tomtom/tomtom_202312.gpkg', bbox=bbox).query("ONEWAY != 'N'"),
-    bbox = bbox,
-    out_folder = "/home/juju/Bureau/",
-    out_file = "grid_healthcare",
-    k = 3,
-    weight_function = lambda feature, length : -1 if feature.KPH==0 else 1.1*length/feature.KPH*3.6,
-    direction_fun = direction_fun,
-    is_not_snappable_fun = lambda f: f.RAMP==1 or f.FOW==5 or (f.FOW==1 and f.FRC==0),
-    initial_node_level_fun = lambda f:f.F_ELEV,
-    final_node_level_fun = lambda f:f.T_ELEV,
-    cell_id_fun = lambda x,y: "CRS3035RES"+str(grid_resolution)+"mN"+str(int(y))+"E"+str(int(x)),
-    grid_resolution= grid_resolution,
-    cell_network_max_distance= grid_resolution * 1.5,
-    partition_size = 250000,
-    extention_buffer = 60000,
-    detailled = True,
-    duration_simplification_fun = lambda x:round(x,1),
-    crs = 'EPSG:3035',
-    num_processors_to_use = 1,
-    save_GPKG = True,
-    save_CSV = False,
-    save_parquet = False
-)
