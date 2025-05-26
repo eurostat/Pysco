f1fb1bc5488b7087fd89ea456e66ebd72904c4e3
jgaffuri <jgaf@proton.me>
2025-05-26 20:30:25 +0200

    a

--- src/accessibility/euro_access.py
+++ src/accessibility/euro_access.py
@@ -87,7 +87,7 @@ for year in ["2023"]:
         # parquet to tiff
 
         # get all parquet files in the output folder
-        files = [os.path.join(f, f) for f in os.listdir(f) if f.endswith('.parquet')]
+        files = [os.path.join(f, f) for f in os.listdir(out_folder_service) if f.endswith('.parquet')]
         if len(files)==0: continue
 
         print("transforming", len(files), "parquuet files into tif for", service, year)
