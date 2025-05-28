8f1bc7dc04d1dd0342c103b3308ac77c25e4b158
jgaffuri <jgaf@proton.me>
2025-05-28 07:49:10 +0200

    a

--- src/accessibility/euro_access.py
+++ src/accessibility/euro_access.py
@@ -23,7 +23,7 @@ for year in ["2023"]:
 
     for service in ["education", "healthcare"]:
 
-        num_processors_to_use = 7 if service == "education" else 5
+        num_processors_to_use = 7 if service == "education" else 4
 
         # ouput folder
         out_folder_service = out_folder + "out_" + service + "_" + year + "/"
