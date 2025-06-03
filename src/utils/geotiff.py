0fb5be2262f33173f093068ea2a53e48e291034f
jgaffuri <jgaf@proton.me>
2025-06-03 12:34:08 +0200

    a

--- src/utils/geotiff.py
+++ src/utils/geotiff.py
@@ -1,8 +1,6 @@
 import rasterio
-from rasterio.mask import mask
 from rasterio.features import geometry_mask
 import geopandas as gpd
-#import numpy as np
 
 
 # apply mask on a geotiff based on some geometries from a vector file
@@ -56,41 +54,4 @@ def geotiff_mask_by_countries(
     # write output tiff
     with rasterio.open(out_tiff_path, 'w', **profile) as dst:
         dst.write(data)
-
-
-
-
-
-
-
-
-def geotiff_mask_by_countries___(
-          in_tiff_path,
-          out_tiff_path,
-          values_to_exclude,
-          gpkg = '/home/juju/geodata/gisco/CNTR_RG_100K_2024_3035.gpkg',
-          gpkg_column = 'CNTR_ID',
-          compress = None
-):
-
-        # load mask geometries
-        gdf = gpd.read_file(gpkg)
-        gdf = gdf[ gdf[gpkg_column].isin(values_to_exclude) ]
-
-        # apply mask
-        with rasterio.open(in_tiff_path) as src:
-            out_image, out_transform = mask(src, gdf.geometry, crop=True, nodata=src.nodata, invert=True)
-            out_meta = src.meta.copy()
-
-        # update metadata
-        out_meta.update({"driver": "GTiff",
-                        "height": out_image.shape[1],
-                        "width": out_image.shape[2],
-                        "transform": out_transform,
-                        "compress": "none" if compress is None else compress
-                        })
-
-        # save output
-        with rasterio.open(out_tiff_path, "w", **out_meta) as dest:
-            dest.write(out_image)
 
