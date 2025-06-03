67eee34b506b105e9870ee1d36dba9643250089f
jgaffuri <jgaf@proton.me>
2025-06-03 11:15:39 +0200

    a

--- src/accessibility/mask_test.py
+++ src/accessibility/mask_test.py
@@ -5,8 +5,8 @@ from utils.geotiff import geotiff_mask_by_countries
 
 
 geotiff_mask_by_countries(
-        '/home/juju/gisco/accessibility/euro_access_education_2023_100m.tif',
-        '/home/juju/gisco/accessibility/euro_access_education_2023_100m_mask.tif',
+        '/home/juju/gisco/accessibility/euro_access_healthcare_2023_100m.tif',
+        '/home/juju/gisco/accessibility/euro_access_healthcare_2023_100m_mask.tif',
         values_to_exclude = ["DE", "CH", "RS", "BA", "MK", "AL", "ME", "MD"],
         gpkg = '/home/juju/geodata/gisco/CNTR_RG_100K_2024_3035.gpkg',
         gpkg_column = 'CNTR_ID',
