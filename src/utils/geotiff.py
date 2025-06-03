840349402e4938147a23ec41854fdc15708dc388
jgaffuri <jgaf@proton.me>
2025-06-03 10:26:12 +0200

    a

--- src/utils/geotiff.py
+++ src/utils/geotiff.py
@@ -19,9 +19,8 @@ def geotiff_mask_by_countries(
 
         # apply mask
         with rasterio.open(in_tiff_path) as src:
-            out_meta = src.meta.copy()
-            print(src.meta)
             out_image, out_transform = mask(src, gdf.geometry, crop=True, nodata=src.nodata)
+            out_meta = src.meta.copy()
 
         # update metadata
         out_meta.update({"driver": "GTiff",
