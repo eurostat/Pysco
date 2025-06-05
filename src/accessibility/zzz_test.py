04923566de8996ffb0f73a1b81e0d19a38b747ad
jgaffuri <jgaf@proton.me>
2025-06-05 08:48:11 +0200

    a

--- src/accessibility/zzz_test.py
+++ src/accessibility/zzz_test.py
@@ -8,6 +8,7 @@ from utils.featureutils import iter_features
 from utils.convert import parquet_grid_to_geotiff
 from utils.geotiff import combine_geotiffs,rename_geotiff_bands
 
+#TODO check cyprus and north
 #TODO produce 1000m
 #TODO healthcare: new 2023 with new EL
 
