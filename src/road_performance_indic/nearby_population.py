40b41f10a445a106800b19dc2821f034241e203b
jgaffuri <jgaf@proton.me>
2025-06-06 10:40:11 +0200

    a

--- src/road_performance_indic/nearby_population.py
+++ src/road_performance_indic/nearby_population.py
@@ -29,7 +29,8 @@ def circular_kernel_sum(
     data = ndimage.convolve(data, kernel, mode='constant', cval=0)
 
     profile.update(dtype=dtype)
-    profile.update(nodata=None)
+    profile.pop("nodata", None)
+    #profile.update(nodata=None)
     if compress is not None:
         profile.update(compress=compress)
 
