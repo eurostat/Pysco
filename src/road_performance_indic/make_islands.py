a520f5eb980476aad488c023f10b7dd31c5f0948
jgaffuri <jgaf@proton.me>
2025-06-06 14:52:16 +0200

    a

--- src/road_performance_indic/make_islands.py
+++ src/road_performance_indic/make_islands.py
@@ -77,7 +77,7 @@ def intersect_with_grid(input_gpkg, grid_resolution, output_gpkg):
 
 ccs = [
     "AT", "BE", "BG", "HR", "CY", "CZ", "DK", "EE", "FI", "FR",
-    "DE", "GR", "HU", "IE", "IT", "LV", "LT", "LU", "MT", "NL",
-    "PL", "PT", "RO", "SK", "SI", "ES", "SE", "AD", "SM", "MC", "VA"
+    "DE", "EL", "HU", "IE", "IT", "LV", "LT", "LU", "MT", "NL",
+    "PL", "PT", "RO", "SK", "SI", "ES", "SE", "AD", "SM", "MC", "VA", "NO", "CH"
 ]
 make_landmass_polygons("/home/juju/geodata/gisco/CNTR_RG_100K_2024_3035.gpkg", "/home/juju/gisco/road_transport_performance/land_mass.gpkg", "CNTR_ID", ccs)
