9f55da252dedd4457d5d10cb88b43c4e71810642
jgaffuri <jgaf@proton.me>
2025-06-07 23:06:31 +0200

    a

--- src/utils/netutils.py
+++ src/utils/netutils.py
@@ -1,6 +1,6 @@
-import networkx as nx
 import math
 from rtree import index
+from shapely.geometry import shape
 
 from utils.geomutils import densify_line
 from collections import defaultdict
@@ -106,8 +106,8 @@ def ___graph_adjacency_list_from_geodataframe(sections_iterator,
             if n1==n2: continue
 
             # get segment weight
-            #TODO when detailled=False, it should be based on the initial line length !
-            segment_length_m = math.hypot(p1[0]-p2[0], p1[1]-p2[1])
+            if detailled: segment_length_m = math.hypot(p1[0]-p2[0], p1[1]-p2[1])
+            else: segment_length_m = shape(geom).length
             w = weight_fun(f, segment_length_m)
             if w<0: continue
 
@@ -133,73 +133,6 @@ def ___graph_adjacency_list_from_geodataframe(sections_iterator,
     return { 'graph':graph, 'snappable_nodes':list(snappable_nodes) }
 
 
-
-
-
-
-# make networkx graph from linear features
-def graph_from_geodataframe(gdf, weight = lambda feature:feature.geometry.length, coord_simp=round, edge_fun = None, detailled=False):
-    graph = nx.Graph()
-    for i, feature in gdf.iterrows():
-        g = feature.geometry
-
-        if(detailled):
-            #make one edge for each segment of the geometry
-
-            #create initial node
-            pi = g.coords[0]
-            ni = str(coord_simp(pi[0])) +'_'+ str(coord_simp(pi[1]))
-
-            #create graph edge for each line segment
-            for i in range(1, len(g.coords)):
-
-                #create final node
-                pf = g.coords[i]
-                nf = str(coord_simp(pf[0])) +'_'+ str(coord_simp(pf[1]))
-
-                if(ni!=nf):
-                    #nodes are different: make edge
-
-                    #compute weight
-                    #segment_length = math.hypot(pi[0]-pf[0],pi[1]-pf[1])
-                    segment_length = distance(ni,nf) #TODO be more efficient here
-                    w = weight(feature, segment_length)
-                    if(w<0): continue
-
-                    #add edge
-                    graph.add_edge(ni, nf, weight=w)
-
-                    #in case there is a need to do some stuff on the newly created edge, such as copying feature data, etc.
-                    if edge_fun != None: edge_fun(graph[pi][pf],feature)
-
-                #initial point becomes final point of the next segment
-                pi=pf
-                ni=nf
-
-        else:
-            #make one single edge, from initial to final vertice of the geometry
-
-            #create initial node
-            pi = g.coords[0]
-            pi = str(coord_simp(pi[0])) +'_'+ str(coord_simp(pi[1]))
-
-            #create final node
-            pf = g.coords[-1]
-            pf = str(coord_simp(pf[0])) +'_'+ str(coord_simp(pf[1]))
-
-            #TODO check both nodes are not the same ?
-
-            #compute weight
-            w = weight(feature, g.length)
-            if(w<0): continue
-
-            #add edge
-            graph.add_edge(pi, pf, weight=w)
-
-            #in case there is a need to do some stuff on the newly created edge, such as copying feature data, etc.
-            if edge_fun != None: edge_fun(graph[pi][pf],feature)
-
-    return graph
 
 
 
---
+++ src/utils/networkxutils.py
@@ -0,0 +1,66 @@
+import networkx as nx
+
+
+# make networkx graph from linear features
+def graph_from_geodataframe(gdf, weight = lambda feature:feature.geometry.length, coord_simp=round, edge_fun = None, detailled=False):
+    graph = nx.Graph()
+    for i, feature in gdf.iterrows():
+        g = feature.geometry
+
+        if(detailled):
+            #make one edge for each segment of the geometry
+
+            #create initial node
+            pi = g.coords[0]
+            ni = str(coord_simp(pi[0])) +'_'+ str(coord_simp(pi[1]))
+
+            #create graph edge for each line segment
+            for i in range(1, len(g.coords)):
+
+                #create final node
+                pf = g.coords[i]
+                nf = str(coord_simp(pf[0])) +'_'+ str(coord_simp(pf[1]))
+
+                if(ni!=nf):
+                    #nodes are different: make edge
+
+                    #compute weight
+                    #segment_length = math.hypot(pi[0]-pf[0],pi[1]-pf[1])
+                    segment_length = distance(ni,nf) #TODO be more efficient here
+                    w = weight(feature, segment_length)
+                    if(w<0): continue
+
+                    #add edge
+                    graph.add_edge(ni, nf, weight=w)
+
+                    #in case there is a need to do some stuff on the newly created edge, such as copying feature data, etc.
+                    if edge_fun != None: edge_fun(graph[pi][pf],feature)
+
+                #initial point becomes final point of the next segment
+                pi=pf
+                ni=nf
+
+        else:
+            #make one single edge, from initial to final vertice of the geometry
+
+            #create initial node
+            pi = g.coords[0]
+            pi = str(coord_simp(pi[0])) +'_'+ str(coord_simp(pi[1]))
+
+            #create final node
+            pf = g.coords[-1]
+            pf = str(coord_simp(pf[0])) +'_'+ str(coord_simp(pf[1]))
+
+            #TODO check both nodes are not the same ?
+
+            #compute weight
+            w = weight(feature, g.length)
+            if(w<0): continue
+
+            #add edge
+            graph.add_edge(pi, pf, weight=w)
+
+            #in case there is a need to do some stuff on the newly created edge, such as copying feature data, etc.
+            if edge_fun != None: edge_fun(graph[pi][pf],feature)
+
+    return graph
