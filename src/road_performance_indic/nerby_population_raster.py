abad0e245de146557ee0c643351a7ee7bea2f7bb
jgaffuri <jgaf@proton.me>
2025-06-08 21:23:16 +0200

    a

--- src/road_performance_indic/nerby_population_raster.py
+++ src/road_performance_indic/nerby_population_raster.py
@@ -1,11 +1,13 @@
 # compute nearby population from raster data, using (fast) convolution
-# faster and simpler, but it cannot handle the case of population from different islands
 
 import rasterio
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from utils.geotiff import geotiff_mask_by_countries, circular_kernel_sum
+
+'''
+# prepare population grids
 
 print("mask", "2018")
 geotiff_mask_by_countries(
@@ -25,14 +27,16 @@ geotiff_mask_by_countries(
     values_to_exclude = ["UK", "RS", "BA", "MK", "AL", "ME"],
     compress="deflate"
 )
+'''
+
+# apply fast convolution - without taking into account land mass index
 
 for year in ["2018", "2021"]:
     print("convolution", year)
     circular_kernel_sum(
         "/home/juju/gisco/road_transport_performance/pop_"+year+".tiff",
-        "/home/juju/gisco/road_transport_performance/nearby_population_"+year+".tiff",
+        "/home/juju/gisco/road_transport_performance/nearby_population_"+year+"_fast.tiff",
         120000,
         rasterio.uint32,
         compress="deflate",
         )
-
