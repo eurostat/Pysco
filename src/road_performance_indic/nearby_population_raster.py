b102fb7fd446a2de1fde44365a202ed20c486732
jgaffuri <jgaf@proton.me>
2025-06-08 23:06:34 +0200

    a

--- src/road_performance_indic/nearby_population_raster.py
+++ src/road_performance_indic/nearby_population_raster.py
@@ -61,8 +61,8 @@ rasterise_tesselation_gpkg(
 
 '''
 
-for year in ["2021", "2018"]:
-
+for year in ["2021"]: #, "2018"
+    '''
     print("combine population + land mass index")
     combine_geotiffs(
         [
@@ -74,7 +74,7 @@ for year in ["2021", "2018"]:
         nodata_value=-9999,
         dtype=np.int64,
     )
-
+    '''
     print("compute convolution")
     circular_kernel_sum_per_code_fast(
         "/home/juju/gisco/road_transport_performance/pop_"+year+"_lmi.tiff",
