02178c292f15768fd933e5fc8231a5515c7dca95
jgaffuri <jgaf@proton.me>
2025-06-09 12:06:57 +0200

    a

--- src/road_performance_indic/nearby_population_raster.py
+++ src/road_performance_indic/nearby_population_raster.py
@@ -52,8 +52,6 @@ for year in ["2018", "2021"]:
 
 resolution = 1000
 
-'''
-# TODO check peloponese !
 print("rasterise land mass index")
 rasterise_tesselation_gpkg(
     "/home/juju/gisco/road_transport_performance/land_mass_gridded.gpkg",
@@ -65,7 +63,7 @@ rasterise_tesselation_gpkg(
     dtype=np.int32,
     all_touched = True
 )
-'''
+
 
 for year in ["2021", "2018"]: #
 
