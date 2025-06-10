32552779828b7941bb883d4a7f83c44417fe7fa2
jgaffuri <jgaf@proton.me>
2025-06-10 14:24:58 +0200

    a

--- src/road_performance_indic/nearby_population_raster.py
+++ src/road_performance_indic/nearby_population_raster.py
@@ -140,7 +140,7 @@ def circular_kernel_sum_per_code(
 
 
 #TODO: 100m: disaggregate 2028
-
+folder = "/home/juju/gisco/road_transport_performance/"
 
 pop = {
     "2018" : "/home/juju/geodata/census/2018/JRC_1K_POP_2018_clean.tif",
@@ -154,7 +154,7 @@ for resolution in [1000]: #100
         print("convolution", year)
         circular_kernel_sum(
             pop[year],
-            "/home/juju/gisco/road_transport_performance/nearby_population_"+year+"_fast.tiff",
+            folder + "nearby_population_"+year+"_fast.tiff",
             120000,
             rasterio.uint32,
             compress="deflate",
@@ -163,8 +163,8 @@ for resolution in [1000]: #100
 
     print("rasterise land mass index")
     rasterise_tesselation_gpkg(
-        "/home/juju/gisco/road_transport_performance/land_mass_gridded.gpkg",
-        "/home/juju/gisco/road_transport_performance/land_mass_gridded.tiff",
+        folder + "land_mass_gridded.gpkg",
+        folder + "land_mass_gridded.tiff",
         fieldname='code',
         resolution=resolution,
         compression='deflate',
@@ -173,16 +173,16 @@ for resolution in [1000]: #100
         all_touched = True
     )
 
-    for year in ["2021", "2018"]: #
+    for year in ["2021", "2018"]:
         print(year)
 
         print("combine population + land mass index")
         combine_geotiffs(
             [
-                "/home/juju/gisco/road_transport_performance/pop_"+year+".tiff",
-                "/home/juju/gisco/road_transport_performance/land_mass_gridded.tiff",
+                pop[year],
+                folder + "land_mass_gridded.tiff",
             ],
-            "/home/juju/gisco/road_transport_performance/pop_"+year+"_lmi.tiff",
+            folder + "pop_"+year+"_lmi.tiff",
             compress="deflate",
             nodata_value=-9999,
             dtype=np.int64,
@@ -190,8 +190,8 @@ for resolution in [1000]: #100
 
         print("compute convolution")
         circular_kernel_sum_per_code(
-            "/home/juju/gisco/road_transport_performance/pop_"+year+"_lmi.tiff",
-            "/home/juju/gisco/road_transport_performance/nearby_population_"+year+".tiff",
+            folder + "pop_"+year+"_lmi.tiff",
+            folder + "nearby_population_"+year+".tiff",
             radius_m=120000,
             dtype=rasterio.int64,
             compress="deflate",
