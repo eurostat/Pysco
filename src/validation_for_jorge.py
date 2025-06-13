3ab246f08d494d56695d384da1e820abe4078db2
jgaffuri <jgaf@proton.me>
2025-06-13 18:12:56 +0200

    a

--- src/validation_for_jorge.py
+++ src/validation_for_jorge.py
@@ -3,6 +3,18 @@ from rtree import index
 from shapely.ops import polygonize, unary_union
 
 
+def check_validity(gpkg_path):
+    print("load")
+    gdf = gpd.read_file(gpkg_path)["geometry"]
+    gdf = gdf.geometry.tolist()
+
+    for g in gdf:
+        v = g.is_valid
+        if v: continue
+        print("Non valide geometry around: ", g.centroid)
+
+
+
 def check_intersections(gpkg_path):
     print("load")
     gdf = gpd.read_file(gpkg_path)
@@ -34,16 +46,18 @@ def check_intersections(gpkg_path):
             print("intersection",inte)
 
 
-def polyg(gpkg_path):
+def check_polygonise(gpkg_path):
 
     # get polygon contours
     gdf = gpd.read_file(gpkg_path)
     gdf = gdf.explode(index_parts=True)
+    print(len(gdf))
     gdf = gdf.geometry.boundary
     gdf = gdf.geometry.tolist()
     print(len(gdf), "lines")
 
     gdf = unary_union(gdf)
+    gdf = list(gdf.geoms)
     print(len(gdf), "lines")
 
     polygons = list(polygonize(gdf))
@@ -51,14 +65,15 @@ def polyg(gpkg_path):
     print(len(polygons), "polygons")
 
     for poly in polygons:
-        poly = poly.buffer(-0.001)
-        if poly.is_empty:
-            print("found")
-            print(poly.centroid)
+        poly_ = poly.buffer(-1)
+        if poly_.is_empty:
+            print("Issue around:", poly.centroid)
 
 
 
 
 gf = "/home/juju/Bureau/jorge_stuff/AU_NO_SE_FI_V.gpkg"
+check_validity(gf)
 #check_intersections(gf)
-polyg(gf)
+#check_polygonise(gf)
+
