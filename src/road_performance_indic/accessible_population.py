bee3cb64102b4abcd2a1f1659b9a0179a1e1abb9
jgaffuri <jgaf@proton.me>
2025-06-14 08:00:21 +0200

    a

--- src/road_performance_indic/accessible_population.py
+++ src/road_performance_indic/accessible_population.py
@@ -317,7 +317,7 @@ for year in ["2021"]:
     def road_network_loader(bbox): return iter_features("/home/juju/geodata/tomtom/tomtom_"+tomtom_year+"12.gpkg", bbox=bbox, where="NOT(FOW==-1 AND FEATTYP==4130)")
     population_grid_loader = population_grid_loader_2021 if year == "2021" else None
 
-    if True:
+    if False:
         accessiblity_population(
                             road_network_loader,
                             bbox = bbox,
