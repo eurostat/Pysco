981ce2651bb90b4852734fd2cee80f608442e637
jgaffuri <jgaf@proton.me>
2025-06-16 15:01:18 +0200

    a

--- src/validation_for_jorge.py
+++ src/validation_for_jorge.py
@@ -4,114 +4,91 @@ from shapely.geometry import Point, LineString
 from shapely.ops import polygonize, unary_union, nearest_points
 
 
-# check all geometries of the input file are valid
-def check_validity(gpkg_path, bbox=None):
-    print("load")
-    gdf = gpd.read_file(gpkg_path, bbox=bbox)["geometry"]
-    gdf = gdf.geometry.tolist()
-    print(len(gdf), "geometries")
-
-    for g in gdf:
-        v = g.is_valid
-        if not v: print("Non valide geometry around: ", g.centroid)
-        # in addition, check effect of the buffer_0.
-        # Buffer_0 operation cleans geometries. It removes duplicate vertices.
-        g_ = g.buffer(0)
-        if count_vertices(g) != count_vertices(g_):
-            print("Issue for geometry around: ", g.centroid)
 
 
 
-# check geometries do not intersect
-def check_intersections(gpkg_path, bbox=None):
-    print("load")
-    gdf = gpd.read_file(gpkg_path, bbox=bbox)
 
-    print("decompose multi polygons to simple polygons")
-    gdf = gdf.explode(index_parts=False)["geometry"]
-    gdf = gdf.geometry.tolist()
-
-    print('make spatial index')
-    items = []
-    for i in range(len(gdf)):
-        g = gdf[i]
-        items.append((i, g.bounds, None))
-    idx = index.Index(((i, box, obj) for i, box, obj in items))
-    del items
-
-    print("check intersection")
-    for i in range(len(gdf)):
-        g = gdf[i]
-        intersl = list(idx.intersection(g.bounds))
-        for j in intersl:
-            if i<=j: continue
-            gj = gdf[j]
-            inte = g.intersects(gj)
-            if not inte: continue
-            inte = g.intersection(gj)
-            if inte.area == 0: continue
-            print("intersection around", inte.centroid, "area=", inte.area)
-
-
-# check polygonisation do not create tiny thin polygons
-def check_polygonise(gpkg_path, epsilon=1, bbox=None):
-
-    print("load and prepare geometries")
-    gdf = gpd.read_file(gpkg_path, bbox=bbox)
-    gdf = gdf.explode(index_parts=True)
-    print(len(gdf), "polygons")
-    gdf = gdf.geometry.boundary
-    gdf = gdf.explode(index_parts=True)
-    gdf = gdf.geometry.tolist()
-    print(len(gdf), "lines")
-
-    # unionise lines, to remove duplicates
-    gdf = unary_union(gdf)
-    gdf = list(gdf.geoms)
-    print(len(gdf), "lines")
-
-    # polygonise lines
-    polygons = list(polygonize(gdf))
-    print(len(polygons), "polygons")
-
-    # check polygons
-    for poly in polygons:
-        poly_ = poly.buffer(-epsilon)
-        if poly_.is_empty:
-            print("Issue around:", poly.centroid)
-
-
-def count_vertices(geometry):
-    if geometry.geom_type == 'Point':
-        return 1
-    elif geometry.geom_type == 'LineString':
-        return len(geometry.coords)
-    elif geometry.geom_type == 'Polygon':
-        exterior_coords = list(geometry.exterior.coords)
-        num_vertices = len(exterior_coords)
-        # Subtract 1 because the first and last vertices of a polygon's exterior ring are the same
-        return num_vertices - 1 if num_vertices > 0 else 0
-    elif geometry.geom_type == 'MultiPolygon':
-        num_vertices = 0
-        for polygon in geometry.geoms:
-            num_vertices += count_vertices(polygon)
-        return num_vertices
-    else:
-        raise ValueError(f"Unsupported geometry type: {geometry.geom_type}")
-
-
-
-def check_noding(gpkg_path, output_gpkg, epsilon = 0.001, bbox=None, detect_microscopic_segments=True, detect_noding=True):
+def validate(gpkg_path, output_gpkg, epsilon = 0.001, bbox=None, detect_microscopic_segments=True, detect_noding=True, check_polygonisation=True, check_sf_validity=True):
+    # list of issues
     issues = []
 
-    print("load and prepare geometries")
+    print("load features")
     gdf = gpd.read_file(gpkg_path, bbox=bbox)
+
+    if check_sf_validity:
+        print("get feature geometries")
+        mps = gdf["geometry"].geometry.tolist()
+        print(len(mps), "features")
+
+        for g in mps:
+            v = g.is_valid
+            if not v: print("Non valid geometry around: ", g.centroid)
+            # in addition, check effect of the buffer_0.
+            # Buffer_0 operation cleans geometries. It removes duplicate vertices.
+            g_ = g.buffer(0)
+            if count_vertices(g) != count_vertices(g_):
+                print("Issue for geometry around: ", g.centroid)
+        del mps
+
+    # convert to line geometries
     gdf = gdf.explode(index_parts=True)
     print(len(gdf), "polygons")
+
+
+    if True:
+        print("decompose multi polygons to simple polygons")
+        polys = gdf.explode(index_parts=False)["geometry"]
+        polys = gdf.geometry.tolist()
+
+        print('make spatial index')
+        items = []
+        for i in range(len(polys)):
+            g = polys[i]
+            items.append((i, g.bounds, None))
+        idx = index.Index(((i, box, obj) for i, box, obj in items))
+        del items
+
+        print("check intersection")
+        for i in range(len(polys)):
+            g = polys[i]
+            intersl = list(idx.intersection(g.bounds))
+            for j in intersl:
+                if i<=j: continue
+                gj = polys[j]
+                inte = g.intersects(gj)
+                if not inte: continue
+                inte = g.intersection(gj)
+                if inte.area == 0: continue
+                print("intersection around", inte.centroid, "area=", inte.area)
+        del polys
+
+
+
     gdf = gdf.geometry.boundary
     gdf = gdf.explode(index_parts=True)
     gdf = gdf.geometry.tolist()
     print(len(gdf), "lines")
+
+
+    if check_polygonisation:
+
+        # unionise lines, to remove duplicates
+        lines = unary_union(gdf)
+        lines = list(lines.geoms)
+        print(len(lines), "lines")
+
+        # polygonise lines
+        polygons = list(polygonize(lines))
+        print(len(polygons), "polygons after polygonisation")
+        del lines
+
+        # check polygons
+        for poly in polygons:
+            poly_ = poly.buffer(-epsilon)
+            if poly_.is_empty:
+                issues.append(["Thin polygon", "thin polygon", poly.centroid])
+        del polygons
+
 
     print("make list of segments and nodes")
     segments = []
@@ -168,9 +145,34 @@ def check_noding(gpkg_path, output_gpkg, epsilon = 0.001, bbox=None, detect_micr
 
 
 
+
+
+def count_vertices(geometry):
+    if geometry.geom_type == 'Point':
+        return 1
+    elif geometry.geom_type == 'LineString':
+        return len(geometry.coords)
+    elif geometry.geom_type == 'Polygon':
+        exterior_coords = list(geometry.exterior.coords)
+        num_vertices = len(exterior_coords)
+        # Subtract 1 because the first and last vertices of a polygon's exterior ring are the same
+        return num_vertices - 1 if num_vertices > 0 else 0
+    elif geometry.geom_type == 'MultiPolygon':
+        num_vertices = 0
+        for polygon in geometry.geoms:
+            num_vertices += count_vertices(polygon)
+        return num_vertices
+    else:
+        raise ValueError(f"Unsupported geometry type: {geometry.geom_type}")
+
+
+
+
+
+
 gf = "/home/juju/Bureau/jorge_stuff/AU_NO_SE_FI_V.gpkg"
 bbox = None #(4580000, 3900000, 4599000, 3970000)
-check_noding(gf, "/home/juju/Bureau/jorge_stuff/issues.gpkg", bbox=bbox)
+validate(gf, "/home/juju/Bureau/jorge_stuff/issues.gpkg", bbox=bbox)
 #check_validity(gf)
 #check_intersections(gf)
 #check_polygonise(gf, bbox=bbox)
