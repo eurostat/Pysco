de30148fc2f034503b69724c3558d6c967030a25
jgaffuri <jgaf@proton.me>
2025-06-16 11:19:07 +0200

    a

--- src/validation_for_jorge.py
+++ src/validation_for_jorge.py
@@ -142,7 +142,7 @@ def check_noding(gpkg_path, output_gpkg, epsilon = 0.001, bbox=None):
 
         print("compute node to segment analysis")
         for seg in segments:
-            candidate_nodes = list(idx.intersection(seg.bounds, 1))
+            candidate_nodes = idx.intersection(seg.bounds, 1)
             #print(len(candidate_nodes))
             for cn in candidate_nodes:
                 cn = nodes[cn]
@@ -160,7 +160,8 @@ def check_noding(gpkg_path, output_gpkg, epsilon = 0.001, bbox=None):
 
 
 gf = "/home/juju/Bureau/jorge_stuff/AU_NO_SE_FI_V.gpkg"
-check_noding(gf, "/home/juju/Bureau/jorge_stuff/issues.gpkg")
+bbox = (4588000, 3902000, 4595000, 3930000)
+check_noding(gf, "/home/juju/Bureau/jorge_stuff/issues.gpkg", bbox=bbox)
 #check_validity(gf)
 #check_intersections(gf)
 #check_polygonise(gf)
