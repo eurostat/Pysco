463d401e60b612402ddd26ceb1b027d959d7023a
jgaffuri <jgaf@proton.me>
2025-06-16 18:01:55 +0200

    a

--- src/utils/gifutils.py
+++ src/utils/gifutils.py
@@ -35,8 +35,9 @@ def create_gif(input_path, input_images, output_gif_path, gif_size=(300, 300), d
     )
 
 
-
+'''
 input_path = "/home/juju/orienteering/omap_thionville_fameck/exports/"
 input_images = [f"{i}.png" for i in range(2, 22)]
 output_gif_path = "/home/juju/Bureau/output.gif"
 create_gif(input_path, input_images, output_gif_path, gif_size=(992, 702), duration=800)
+'''
