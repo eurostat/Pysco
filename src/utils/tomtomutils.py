104c20ba77088f9a0fac611c5b48d4009f240841
jgaffuri <jgaf@proton.me>
2025-06-16 22:12:40 +0200

    a

--- src/utils/tomtomutils.py
+++ src/utils/tomtomutils.py
@@ -7,23 +7,37 @@ def direction_fun(feature):
     if d=="FT": return 'forward'
     if d=="TF": return 'backward'
     if d=="N": return 'both'
-    print("Unexpected driving direction: ", d)
+    print("Unexpected driving direction:", d)
     return None
 
+
+# the weight of a length of a road, for routing. This is the estimated driving time.
 def weight_function(feature, length):
     p = feature['properties']
     kph = 0
+
     # ferry
     if p['FOW']==-1 and p['FEATTYP']==4130: kph = 30
     # private/restricted/pedestrian roads
     elif p['ONEWAY']=='N': kph = 15
     # default case
     else: kph = p['KPH']
-    if kph==0: return -1
-    # duration in seconds
+
+    # non drivable case
+    if kph == 0: return -1
+
+    # duration in seconds, rounded, based on the speed. Plus 10% assuming kph is the maximum speed with no traffic and no stops.
     return round(1.1 * length / kph * 3.6)
+
+
+# return wether a section cannot be used as access point. Residential roads can, highways and ferry lines cannot.
 def is_not_snappable_fun(f):
     p = f['properties']
     return p['FOW'] in [1,10,12,6] or p['FREEWAY'] == 1 or (p['FOW']==-1 and p['FEATTYP']==4130)
+
+
+# code to tag the level of the initial node of the section
 def initial_node_level_fun(f): return f['properties']['F_ELEV']
+# code to tag the level of the final node of the section
 def final_node_level_fun(f): return f['properties']['T_ELEV']
+
