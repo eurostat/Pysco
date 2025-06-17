0666b695c3ac198f7e71c424c77bc5890fe54497
jgaffuri <jgaf@proton.me>
2025-06-17 11:00:46 +0200

    a

--- src/road_performance_indic/combine.py
+++ src/road_performance_indic/combine.py
@@ -7,9 +7,7 @@ year = "2021"
 grid_resolution = 1000
 
 geotiff_ap = out_folder + "accessible_population_" + year + "_" + str(grid_resolution) + "m.tif"
-geotiff_np = out_folder + "accessible_population_" + year + "_" + str(grid_resolution) + "m.tif"
-
-
+geotiff_np = out_folder + "nearby_population_" + year + "_" + str(grid_resolution) + "m.tif"
 
 print("rename tiff bands")
 rename_geotiff_bands(geotiff, [service + "_" + year + "_1", service + "_" + year + "_a3"])
