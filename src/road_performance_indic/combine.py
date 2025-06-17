6b318632868e71318b8d971655315f85384f3da0
jgaffuri <jgaf@proton.me>
2025-06-17 11:05:32 +0200

    a

--- src/road_performance_indic/combine.py
+++ src/road_performance_indic/combine.py
@@ -1,6 +1,9 @@
 
 # combine all tiff into a single one
-# band on 
+# band on nearby pop, accessible pop, ratio
+
+from utils.geotiff import combine_geotiffs
+
 
 out_folder = '/home/juju/gisco/road_transport_performance/'
 year = "2021"
@@ -8,7 +11,12 @@ grid_resolution = 1000
 
 geotiff_ap = out_folder + "accessible_population_" + year + "_" + str(grid_resolution) + "m.tif"
 geotiff_np = out_folder + "nearby_population_" + year + "_" + str(grid_resolution) + "m.tif"
+combined = out_folder + "combined_" + year + "_" + str(grid_resolution) + "m.tif"
 
-print("rename tiff bands")
-rename_geotiff_bands(geotiff, [service + "_" + year + "_1", service + "_" + year + "_a3"])
+print("combine geotiff")
+combine_geotiffs([geotiff_np, geotiff_ap], combined, compress="deflate")
+
+
+#print("rename tiff bands")
+#rename_geotiff_bands(geotiff, [service + "_" + year + "_1", service + "_" + year + "_a3"])
 
