95af5e76e3bb23e111f637ebb56a421e6468cdd1
jgaffuri <jgaf@proton.me>
2025-06-19 07:55:43 +0200

    a

--- src/road_performance_indic/accessible_population.py
+++ src/road_performance_indic/accessible_population.py
@@ -324,7 +324,7 @@ for year in ["2018"]:
     #TODO take right tomtom version
     def road_network_loader(bbox): return iter_features("/home/juju/geodata/tomtom/tomtom_"+tomtom_year+"12.gpkg", bbox=bbox, where="NOT(FOW==-1 AND FEATTYP==4130)")
 
-    if True:
+    if False:
         accessiblity_population_parallel(
                             road_network_loader,
                             population_grid_loader_2021 if year == "2021" else population_grid_loader_2018,
