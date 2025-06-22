37d4673d881e688a2efc187200066157e3fff3bf
jgaffuri <jgaf@proton.me>
2025-06-22 21:58:44 +0200

    a

--- src/road_performance_indic/gridviz_preparation.py
+++ src/road_performance_indic/gridviz_preparation.py
@@ -8,11 +8,22 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from utils.geotiff import combine_geotiffs, resample_geotiff_aligned
 
 
+
 f0 = "/home/juju/gisco/road_transport_performance/"
 folder = f0 + "gridviz/"
 
+
+def aggregate():
+    for year in ["2018","2021"]:
+        for indicator in ["nearby_population", "accessible_population"]:
+            for resolution in [1000, 2000, 5000, 10000, 20000, 50000, 100000]:
+                print("aggregate", year, indicator, resolution)
+                infile = f0 + "nearby_population_" + year + "_1000m.tif"
+                resample_geotiff_aligned(infile, folder+indicator+"_" +year+"_"+str(resolution)+".tif", resolution, Resampling.average)
+
+
 def tiling():
-    for indicator in ["np", "ap", "rp"]:
+    for indicator in ["np", "ap"]:
         for resolution in [1000, 2000, 5000, 10000, 20000, 50000, 100000]:
             folder_ = folder + indicator + "_" + resolution + "/"
             if not os.path.exists(folder): os.makedirs(folder)
@@ -23,3 +34,4 @@ def tiling():
             pop_file = "/home/juju/geodata/census/"+year+"/aggregated_tiff/pop_"+year+"_"+resolution+".tif"
 
 
+aggregate()
