bc32afd50f1d18c2d2b47c4d323e4b5afa724bf4
jgaffuri <jgaf@proton.me>
2025-06-22 22:09:22 +0200

    a

--- src/road_performance_indic/gridviz_preparation.py
+++ src/road_performance_indic/gridviz_preparation.py
@@ -5,28 +5,37 @@ import os
 from rasterio.enums import Resampling
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from utils.geotiff import combine_geotiffs, resample_geotiff_aligned
+from utils.geotiff import combine_geotiffs, resample_geotiff_aligned, add_ratio_band
 
 
 
 f0 = "/home/juju/gisco/road_transport_performance/"
 folder = f0 + "gridviz/"
+if not os.path.exists(folder): os.makedirs(folder)
 
 
 def aggregate():
     for year in ["2018","2021"]:
-        for indicator in ["nearby_population", "accessible_population"]:
-            for resolution in [1000, 2000, 5000, 10000, 20000, 50000, 100000]:
+        for resolution in [1000, 2000, 5000, 10000, 20000, 50000, 100000]:
+            for indicator in ["nearby_population", "accessible_population"]:
                 print("aggregate", year, indicator, resolution)
-                infile = f0 + "nearby_population_" + year + "_1000m.tif"
-                resample_geotiff_aligned(infile, folder+indicator+"_" +year+"_"+str(resolution)+".tif", resolution, Resampling.average)
+                resample_geotiff_aligned(
+                    f0 + "nearby_population_" + year + "_1000m.tif",
+                    folder+indicator+"_" +year+"_"+str(resolution)+".tif",
+                    resolution, Resampling.average)
+            np = folder+"nearby_population"+"_" +year+"_"+str(resolution)+".tif"
+            ap = folder+"accessible_population"+"_" +year+"_"+str(resolution)+".tif"
+            rp = folder+"road_performance"+"_" +year+"_"+str(resolution)+".tif"
+            combine_geotiffs([np, ap], rp, compress="deflate", dtype=np.int64)
+            add_ratio_band(rp, 2, 1)
+            #TODO delete np and ap?
 
 
 def tiling():
-    for indicator in ["np", "ap"]:
+    for indicator in ["nearby_population", "accessible_population"]:
         for resolution in [1000, 2000, 5000, 10000, 20000, 50000, 100000]:
             folder_ = folder + indicator + "_" + resolution + "/"
-            if not os.path.exists(folder): os.makedirs(folder)
+            if not os.path.exists(folder_): os.makedirs(folder_)
 
             year = "2021"
             tiff = folder + "road_performance_"+year+"_1000m.tif"
@@ -34,4 +43,6 @@ def tiling():
             pop_file = "/home/juju/geodata/census/"+year+"/aggregated_tiff/pop_"+year+"_"+resolution+".tif"
 
 
-aggregate()
+#aggregate()
+tiling()
+
