1e0a0000ac9ba06616ce750ebafe5fb6ef76915e
jgaffuri <jgaf@proton.me>
2025-06-23 18:37:44 +0200

    a

--- src/road_performance_indic/gridviz/gridviz_preparation.py
+++ src/road_performance_indic/gridviz/gridviz_preparation.py
@@ -4,7 +4,7 @@ import sys
 import os
 from rasterio.enums import Resampling
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 from utils.geotiff import combine_geotiffs, resample_geotiff_aligned, add_ratio_band
 
 
