e33b53279020b1efd67810d00777a51a806562cb
jgaffuri <jgaf@proton.me>
2025-06-24 10:39:05 +0200

    a

--- src/accessibility/combine.py
+++ src/accessibility/combine.py
@@ -13,53 +13,53 @@ out_folder = '/home/juju/gisco/accessibility/'
 bbox = [ 900000, 900000, 6600000, 5500000 ]
 
 
-grid_resolution = 100
+for grid_resolution in [1000, 100]:
 
-for service in ["education", "healthcare"]:
+    for service in ["education", "healthcare"]:
 
-    for year in ["2023","2020"]:
+        for year in ["2023","2020"]:
 
-        # ouput folder
-        out_folder_service_year = out_folder + "out_" + service + "_" + year + "_" + str(grid_resolution) + "m/"
-        if not os.path.exists(out_folder_service_year): os.makedirs(out_folder_service_year)
+            # ouput folder
+            out_folder_service_year = out_folder + "out_" + service + "_" + year + "_" + str(grid_resolution) + "m/"
+            if not os.path.exists(out_folder_service_year): continue
 
-        # combine parquet files to a single tiff file
-        geotiff = out_folder + "euro_access_" + service + "_" + year + "_" + str(grid_resolution) + "m.tif"
+            # combine parquet files to a single tiff file
+            geotiff = out_folder + "euro_access_" + service + "_" + year + "_" + str(grid_resolution) + "m.tif"
 
-        # check if tiff file was already produced
-        if os.path.isfile(geotiff): continue
+            # check if tiff file was already produced
+            if os.path.isfile(geotiff): continue
 
-        # get all parquet files in the output folder
-        files = [os.path.join(out_folder_service_year, f) for f in os.listdir(out_folder_service_year) if f.endswith('.parquet')]
-        if len(files)==0: continue
+            # get all parquet files in the output folder
+            files = [os.path.join(out_folder_service_year, f) for f in os.listdir(out_folder_service_year) if f.endswith('.parquet')]
+            if len(files)==0: continue
 
-        print("transforming", len(files), "parquet files into tif for", service, year)
-        parquet_grid_to_geotiff(
-            files,
-            geotiff,
-            bbox = bbox,
-            attributes=["duration_s_1", "duration_average_s_3"],
-            parquet_nodata_values=[-1],
-            dtype=np.int16,
-            value_fun= lambda v:v if v<32767 else 32767, # np.int16(v),
-            compress='deflate'
-        )
+            print(grid_resolution, service, year, "transforming", len(files), "parquet files into tif for", service, year)
+            parquet_grid_to_geotiff(
+                files,
+                geotiff,
+                bbox = bbox,
+                attributes=["duration_s_1", "duration_average_s_3"],
+                parquet_nodata_values=[-1],
+                dtype=np.int16,
+                value_fun= lambda v:v if v<32767 else 32767, # np.int16(v),
+                compress='deflate'
+            )
 
-        print("apply mask to force some countries to nodata")
-        cnts = ["AT", "BE", "BG", "HR", "CY", "CZ", "DK", "EE", "FI", "FR",
-                "EL", "HU", "IE", "IT", "LV", "LT", "LU", "MT", "NL",
-                "PL", "PT", "RO", "SK", "SI", "ES", "SE", "NO"]
-        #exclude: ["DE", "CH", "RS", "BA", "MK", "AL", "ME", "MD"],
-        if service == "healthcare": cnts.append("CH")
-        geotiff_mask_by_countries(
-            geotiff,
-            geotiff,
-            gpkg = '/home/juju/geodata/gisco/CNTR_RG_100K_2024_3035.gpkg',
-            gpkg_column = 'CNTR_ID',
-            values = cnts,
-            compress="deflate"
-        )
+            print(grid_resolution, service, year, "apply mask to force some countries to nodata")
+            cnts = ["AT", "BE", "BG", "HR", "CY", "CZ", "DK", "EE", "FI", "FR",
+                    "EL", "HU", "IE", "IT", "LV", "LT", "LU", "MT", "NL",
+                    "PL", "PT", "RO", "SK", "SI", "ES", "SE", "NO"]
+            #exclude: ["DE", "CH", "RS", "BA", "MK", "AL", "ME", "MD"],
+            if service == "healthcare": cnts.append("CH")
+            geotiff_mask_by_countries(
+                geotiff,
+                geotiff,
+                gpkg = '/home/juju/geodata/gisco/CNTR_RG_100K_2024_3035.gpkg',
+                gpkg_column = 'CNTR_ID',
+                values = cnts,
+                compress="deflate"
+            )
 
-        print("rename tiff bands")
-        rename_geotiff_bands(geotiff, [service + "_" + year + "_1", service + "_" + year + "_a3"])
+            print(grid_resolution, service, year, "rename tiff bands")
+            rename_geotiff_bands(geotiff, [service + "_" + year + "_1", service + "_" + year + "_a3"])
 
