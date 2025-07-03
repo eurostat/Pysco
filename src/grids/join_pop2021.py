797d588afc49f3e9c69d7a2c282b7dff396d7bc7
jgaffuri <jgaf@proton.me>
2025-07-03 09:25:59 +0200

    a

---
+++ src/grids/join_pop2021.py
@@ -0,0 +1,37 @@
+
+import geopandas as gpd
+
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from utils.geotiff import read_geotiff_pixels_as_dicts
+from utils.gridutils import get_cell_id
+
+
+
+for res in ["10"]:
+
+    grid = "/home/juju/geodata/gisco/grids/grid_"+res+"km_surf.gpkg"
+    pop_2021 = "/home/juju/geodata/census/2021/aggregated_tiff/pop_2021_"+res+"000.tif"
+
+    print("load gpkg grid", res+"000m")
+    gdf = gpd.read_file(grid)
+    #print(gdf.head())
+    #print(gdf['TOT_P_2021'].tolist())
+
+    print("load tiff pop 2021", res+"000m")
+    pop_data = read_geotiff_pixels_as_dicts(pop_2021)
+    #print(pop_data)
+
+    # make pop_data grid id
+    pop = {}
+    for d in pop_data:
+        id = get_cell_id(res+"000", "3035", d["x"], d["y"])
+        pop[id] = int(d["value"])
+    del pop_data
+    print(pop)
+
+
+    # set population to 0 everywhere
+    gdf['TOT_P_2021'] = 0
