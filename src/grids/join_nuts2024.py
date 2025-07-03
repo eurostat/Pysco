08176eb788fdc5fc148a74fde8da3c41f41dc7d7
jgaffuri <jgaf@proton.me>
2025-07-03 10:39:28 +0200

    a

--- src/grids/join_nuts2024.py
+++ src/grids/join_nuts2024.py
@@ -7,8 +7,8 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
-
-nuts = "/home/juju/geodata/gisco/NUTS_RG_100K_2024_3035.gpkg"
+nuts_version = "2024"
+nuts = "/home/juju/geodata/gisco/NUTS_RG_100K_"+nuts_version+"_3035.gpkg"
 
 print("load nuts regions")
 nuts = gpd.read_file(nuts)
@@ -16,15 +16,24 @@ nuts = gpd.read_file(nuts)
 
 for res in ["100"]: #, "50", "20", "10", "5", "2", "1"]:
 
-    grid = "/home/juju/geodata/gisco/grids/grid_"+res+"km_surf.gpkg"
-    grid = gpd.read_file(grid)
+    grid_path = "/home/juju/geodata/gisco/grids/grid_"+res+"km_surf.gpkg"
+    grid = gpd.read_file(grid_path)
     print(len(grid))
-
 
     for lev in ["3", "2", "1", "0"]:
         nuts_lev = nuts[nuts['STAT_LEVL_CODE'] == int(lev)]
         #print(len(nuts_lev))
 
+    def fun(cell):
+        return "TODO"
         #"NUTS_ID"
 
+    grid['NUTS_'+nuts_version] = grid.apply(
+        fun,
+        axis=1
+    )
+
+    print("save", res)
+    if os.path.exists(grid_path): os.remove(grid_path)
+    grid.to_file(f, driver="GPKG")
 
