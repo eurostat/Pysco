a23a80a1c6d6504a8c3a0625f8e6e238d53010ae
jgaffuri <jgaf@proton.me>
2025-07-03 10:44:09 +0200

    a

--- src/grids/join_nuts2024.py
+++ src/grids/join_nuts2024.py
@@ -1,12 +1,11 @@
 
 import geopandas as gpd
-
 import sys
 import os
+#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-
+dist = 1500 # get nuts regions within 1.5 km
 nuts_version = "2024"
 nuts = "/home/juju/geodata/gisco/NUTS_RG_100K_"+nuts_version+"_3035.gpkg"
 
@@ -24,14 +23,15 @@ for res in ["100"]: #, "50", "20", "10", "5", "2", "1"]:
         nuts_lev = nuts[nuts['STAT_LEVL_CODE'] == int(lev)]
         #print(len(nuts_lev))
 
-    def fun(cell):
-        return "TODO"
-        #"NUTS_ID"
+        def fun(cell):
+            return "TODO"
+            # get NUTS_ID within dist
+            #"NUTS_ID"
 
-    grid['NUTS_'+nuts_version] = grid.apply(
-        fun,
-        axis=1
-    )
+        grid['NUTS_' + nuts_version + "_" + lev] = grid.apply(
+            fun,
+            axis=1
+        )
 
     print("save", res, "km")
     if os.path.exists(grid_path): os.remove(grid_path)
