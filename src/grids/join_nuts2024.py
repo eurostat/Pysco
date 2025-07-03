3d861fb0ad92a7dd379e21d16f4802ad69ef0836
jgaffuri <jgaf@proton.me>
2025-07-03 10:34:44 +0200

    a

--- src/grids/join_nuts2024.py
+++ src/grids/join_nuts2024.py
@@ -12,12 +12,18 @@ nuts = "/home/juju/geodata/gisco/NUTS_RG_100K_2024_3035.gpkg"
 
 print("load nuts regions")
 nuts = gpd.read_file(nuts)
-print(len(nuts))
+#print(len(nuts))
 
 for res in ["100"]: #, "50", "20", "10", "5", "2", "1"]:
+
+    grid = "/home/juju/geodata/gisco/grids/grid_"+res+"km_surf.gpkg"
+    grid = gpd.read_file(grid)
+    print(len(grid))
+
+
     for lev in ["3", "2", "1", "0"]:
-        nuts_lev = nuts[nuts['STAT_LEVL_CODE'] == lev]
-        print(len(nuts_lev))
+        nuts_lev = nuts[nuts['STAT_LEVL_CODE'] == int(lev)]
+        #print(len(nuts_lev))
 
         #"NUTS_ID"
 
