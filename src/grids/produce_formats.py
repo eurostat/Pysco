0ac21a0892029cf3105a2138cc0bd993adc93c11
jgaffuri <jgaf@proton.me>
2025-07-03 09:45:33 +0200

    a

--- src/grids/produce_formats.py
+++ src/grids/produce_formats.py
@@ -1,3 +1,18 @@
 
+import geopandas as gpd
 
+folder = "/home/juju/geodata/gisco/grids/"
+for res in ["100"]: #, "50", "20", "10", "5", "2", "1"]:
+
+    grid = folder + "grid_"+res+"km_surf.gpkg"
+
+    print("load gpkg grid", res+"000m")
+    gdf = gpd.read_file(grid)
+
+    print("save CSV", res+"000m")
+    gdf = gdf.drop(columns='geometry')
+    gdf.to_csv(folder + "grid_"+res+"km.csv", index=False)
+
+    print("save parquet", res+"000m")
+    gdf.to_parquet(folder + "grid_"+res+"km.parquet", index=False)
 
