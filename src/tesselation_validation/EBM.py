0463938242155f776451b9734dcc4e093b620b61
jgaffuri <jgaf@proton.me>
2025-07-04 11:39:33 +0200

    a

--- src/tesselation_validation/EBM.py
+++ src/tesselation_validation/EBM.py
@@ -1,6 +1,8 @@
 
 
-
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from tesselation_validation.validation import validate_polygonal_tesselation
 
 
