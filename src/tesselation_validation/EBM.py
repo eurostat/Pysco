ac4767e4b56f8f8c65c0998696ee7a68cb916da3
jgaffuri <jgaf@proton.me>
2025-07-04 11:40:17 +0200

    a

--- src/tesselation_validation/EBM.py
+++ src/tesselation_validation/EBM.py
@@ -7,12 +7,13 @@ from tesselation_validation.validation import validate_polygonal_tesselation
 
 
 folder = "/home/juju/geodata/eurogeographics/EBM/"
+out_folder = "/home/juju/Bureau/"
 version = "2025_2"
 for case in ["NUTS_1", "NUTS_2", "NUTS_3", "LAU", "A"]:
     print("*******", case)
     validate_polygonal_tesselation(
                 folder + "EBM_"+case+"_"+version+".gpkg",
-                "/home/juju/Bureau/issues_"+case+"_"+version+".gpkg",
+                out_folder + "issues_"+case+"_"+version+".gpkg",
                 bbox = None, #(4500000, 3900000, 4600000, 3970000),
                 epsilon = 0.01,
                 check_ogc_validity=True,
