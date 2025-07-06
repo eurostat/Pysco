1d4a29ec2f8ceb1ce6ddba047246721cd8944023
jgaffuri <jgaf@proton.me>
2025-07-06 13:04:52 +0200

    a

--- src/tesselation_validation/EBM.py
+++ src/tesselation_validation/EBM.py
@@ -12,31 +12,32 @@ version = "2025_2"
 for case in ["A", "NUTS_1", "NUTS_2", "NUTS_3", "LAU"]:
     print("*******", case)
 
+    '''
     validate_polygonal_tesselation(
                 folder + "EBM_"+case+"_"+version+".gpkg",
                 out_folder + "issues_"+case+"_"+version+"_check_ogc_validity.gpkg",
-                epsilon = epsilon,
                 check_ogc_validity=True,
                 )
+    '''
 
     validate_polygonal_tesselation(
                 folder + "EBM_"+case+"_"+version+".gpkg",
                 out_folder + "issues_"+case+"_"+version+"_check_intersection.gpkg",
-                epsilon = epsilon,
+                epsilon = 0.0001,
                 check_intersection=True,
                 )
 
     validate_polygonal_tesselation(
                 folder + "EBM_"+case+"_"+version+".gpkg",
                 out_folder + "issues_"+case+"_"+version+"_check_microscopic_segments.gpkg",
-                epsilon = epsilon,
+                epsilon = 0.1,
                 check_microscopic_segments=True,
                 )
 
     validate_polygonal_tesselation(
                 folder + "EBM_"+case+"_"+version+".gpkg",
                 out_folder + "issues_"+case+"_"+version+"_check_noding_issues.gpkg",
-                epsilon = epsilon,
+                epsilon = 0.5,
                 check_noding_issues=True,
                 )
 
