fc9bff89d3f270d6424ed493b15cfa363555e267
jgaffuri <jgaf@proton.me>
2025-07-08 10:15:48 +0200

    a

--- src/road_performance_indic/__convert.py
+++ src/road_performance_indic/__convert.py
@@ -7,7 +7,6 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from utils.convert import parquet_grid_to_geotiff
 
 
-
 df = pd.read_csv("/home/juju/gisco/road_transport_performance/grid_network_perf_2011_2019_2021_c.csv")
 #x,y,grd_id,pop120km_2011,pop90m_2011,pop90m_u120_2011,pop120km_2018,pop90m_2019,pop90m_u120_2019,pop120km_2021,pop90m_2022,pop90m_u120_2022
 df = df.drop(columns=["x", "y"])
@@ -22,3 +21,4 @@ parquet_grid_to_geotiff(
     dtype=np.int64,
     compress="deflate",
     )
+
