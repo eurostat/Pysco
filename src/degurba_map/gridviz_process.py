228c8ec107e8ecc70fef32073148b8bc437022a2
jgaffuri <jgaf@proton.me>
2025-09-15 14:03:26 +0200

    a

--- src/degurba_map/gridviz_process.py
+++ src/degurba_map/gridviz_process.py
@@ -10,5 +10,5 @@ from utils.geotiff import resample_geotiff_aligned
 path = "/home/juju/gisco/degurba/"
 
 new_resolution = 2000
-resample_geotiff_aligned(path + "GHS-DUG_GRID_L2.tif", path + "/out/", new_resolution, resampling=Resampling.mode, dtype=np.int8)
+resample_geotiff_aligned(path + "GHS-DUG_GRID_L2.tif", path + "/out/"+str(new_resolution)+".tif", new_resolution, resampling=Resampling.mode, dtype=np.int8)
 
