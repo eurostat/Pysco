128c4b67aa069d3987f9b57be7c164a13b0c9dbf
jgaffuri <jgaf@proton.me>
2025-11-16 11:17:13 +0100

    a

--- src/accessibility/euro_access.py
+++ src/accessibility/euro_access.py
@@ -28,7 +28,7 @@ bbox = [ 900000, 900000, 6600000, 5500000 ]
 
 for grid_resolution in [100]: # 1000
 
-    for year in ["2020", "2023"]: #"2023"
+    for year in ["2023", "2020"]: #"2023"
         for service in ["education", "healthcare"]:
 
             # detailled network decomposition only when resolution to 100m
