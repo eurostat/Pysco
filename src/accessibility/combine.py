9c03422669a28490ca5452ee8fa46ced3525ea39
jgaffuri <jgaf@proton.me>
2025-11-18 10:16:58 +0100

    a

--- src/accessibility/combine.py
+++ src/accessibility/combine.py
@@ -13,6 +13,7 @@ country_gpkg = '/home/juju/geodata/gisco/CNTR_RG_100K_2024_3035.gpkg'
 services = ["healthcare"] #education healthcare
 years = ["2020"] #"2023" 2020
 version_tag = "2025_11"
+skip_combination = True
 
 # whole europe
 bbox = [ 900000, 900000, 6600000, 5500000 ]
@@ -32,29 +33,30 @@ for resolution in [100]:
             geotiff = out_folder + "euro_access_" + service + "_" + year + "_" + str(resolution) + "m_"+version_tag+".tif"
 
             # check if tiff file was already produced
-            if os.path.isfile(geotiff):
+            if os.path.isfile(geotiff) and not skip_combination:
                 print("Combined file already produced")
                 continue
 
-            # get all parquet files in the output folder
-            files = [os.path.join(out_folder_service_year, f) for f in os.listdir(out_folder_service_year) if f.endswith('.parquet')]
-            if len(files)==0:
-                print("No file to combine")
-                continue
+            if not skip_combination:
+                # get all parquet files in the output folder
+                files = [os.path.join(out_folder_service_year, f) for f in os.listdir(out_folder_service_year) if f.endswith('.parquet')]
+                if len(files)==0:
+                    print("No file to combine")
+                    continue
 
-            print(resolution, service, year, "transforming", len(files), "parquet files into tif for", service, year)
-            parquet_grid_to_geotiff(
-                files,
-                geotiff,
-                bbox = bbox,
-                attributes=["duration_s_1", "duration_average_s_3"],
-                parquet_nodata_values=[-1],
-                dtype=np.int16,
-                value_fun= lambda v:v if v<32767 else 32767, # np.int16(v),
-                compress='deflate'
-            )
-            files.clear()
-            files = None
+                print(resolution, service, year, "transforming", len(files), "parquet files into tif for", service, year)
+                parquet_grid_to_geotiff(
+                    files,
+                    geotiff,
+                    bbox = bbox,
+                    attributes=["duration_s_1", "duration_average_s_3"],
+                    parquet_nodata_values=[-1],
+                    dtype=np.int16,
+                    value_fun= lambda v:v if v<32767 else 32767, # np.int16(v),
+                    compress='deflate'
+                )
+                files.clear()
+                files = None
 
             print(resolution, service, year, "apply mask to force some countries to nodata")
             cnts = ["AT", "BE", "BG", "HR", "CY", "CZ", "DE", "DK", "EE", "FI", "FR",
