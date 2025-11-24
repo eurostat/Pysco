acaddb60d6acdfb4cce13e7c519aa1e5ec182a13
jgaffuri <jgaf@proton.me>
2025-11-24 20:17:25 +0100

    a

--- src/census2021/resampling/process.py
+++ src/census2021/resampling/process.py
@@ -1,9 +1,16 @@
 import numpy as np
-#import rasterio
 import shapely.geometry
 import geopandas as gpd
 from rtree import index
+import rasterio
+from rasterio.features import shapes
+from shapely.geometry import shape
 
+
+# use strandlina_flakar for land area
+#TODO nearest neighbor: input to point, then 2
+#TODO test GHSL
+#TODO OSM buildings ?
 
 
 # from a shapely geometry, make n random point geometries within the area
@@ -143,6 +150,57 @@ def dasymetric_aggregation_step_2(input_das_gpkg, pop_att, output_gpkg):
 
 
 
+def raster_pixels_above_threshold_to_gpkg(
+        tiff_paths, threshold, output_gpkg, layer_name="pixels"):
+    """
+    Extracts pixels > threshold from one or more GeoTIFF rasters and
+    saves each pixel as a polygon feature in a GeoPackage.
+
+    Parameters
+    ----------
+    tiff_paths : list of str
+        List of paths to GeoTIFF files.
+    threshold : float
+        Pixel threshold value.
+    output_gpkg : str
+        Path of output GeoPackage to create.
+    layer_name : str
+        Name of layer inside GPKG.
+    """
+
+    all_geoms = []
+    all_vals = []
+
+    for path in tiff_paths:
+        with rasterio.open(path) as src:
+            data = src.read(1)
+            mask = data > threshold
+
+            # shapes() yields (geometry, value) pairs
+            for geom, val in shapes(data, mask=mask, transform=src.transform):
+                all_geoms.append(shape(geom))
+                all_vals.append(float(val))
+
+    gdf = gpd.GeoDataFrame(
+        {"value": all_vals},
+        geometry=all_geoms,
+        crs=src.crs
+    )
+
+    # Write to GeoPackage
+    gdf.to_file(output_gpkg, layer=layer_name, driver="GPKG")
+
+    return gdf
+
+
+
+# GHSL to vector
+raster_pixels_above_threshold_to_gpkg(
+    ['/home/juju/geodata/IS/GHS_BUILT_S_E2020_GLOBE_R2023A_54009_100_V1_0_R2_C17.tif', '/home/juju/geodata/IS/GHS_BUILT_S_E2020_GLOBE_R2023A_54009_100_V1_0_R2_C18.tif'],
+    1, w+'out/ghsl.gpkg')
+
+
+'''
 w = '/home/juju/gisco/census_2021_iceland/'
 
 dasymetric_disaggregation_step_1(
@@ -155,7 +213,7 @@ dasymetric_disaggregation_step_1(
 
 dasymetric_aggregation_step_2(w+"out/disag_area.gpkg", "popul", w+"out/ag_area.gpkg")
 dasymetric_aggregation_step_2(w+"out/disag_point.gpkg", None, w+"out/ag_point.gpkg")
-
+'''
 
 
 
