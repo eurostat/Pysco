68debb477a09ab1c243b4a6f4a3ac21b87669864
jgaffuri <jgaf@proton.me>
2025-11-27 19:10:35 +0100

    a

--- src/census2021/resampling/process.py
+++ src/census2021/resampling/process.py
@@ -76,7 +76,7 @@ def make_synthetic_population(n, data, check_counts=True):
     return population
 
 
-def count_categories(population, categories, sort=True):
+def count_categories(population, categories, tot="count", sort=True):
     """
     population : list of dicts produced by your synthetic generator
     categories     : list of category names to count
@@ -95,7 +95,8 @@ def count_categories(population, categories, sort=True):
             if mode in stats: stats[mode] = stats[mode] + 1
             else: stats[mode] = 1
     # sort
-    if sort: return { k: stats[k] for k in sorted(stats) }
+    if sort: stats = { k: stats[k] for k in sorted(stats) }
+    stats[tot] = len(population)
     return stats
 
 
@@ -164,8 +165,6 @@ def dasymetric_disaggregation_step_1(input_pop_gpkg, input_dasymetric_gpkg, pop_
     gpd.GeoDataFrame(output_areas, geometry='geometry', crs=gdf.crs).to_file(output_gpkg, driver='GPKG')
 
     if output_synthetic_population_gpkg is not None:
-        a = count_categories(output_synthetic_population, ["sex", "age_g", "pob_l", "roy"])
-        print(a)
         # save output points
         gpd.GeoDataFrame(output_synthetic_population, geometry='geometry', crs=gdf.crs).to_file(output_synthetic_population_gpkg, driver='GPKG')
 
@@ -281,84 +280,4 @@ print("Dasymetric aggregation step 2 from points")
 
 #print("Nearest neighbour")
 #dasymetric_aggregation_step_2(w+"IS_pop_grid_point_3035.gpkg", "sex_0", w+"out/nearest_neighbour.gpkg")
-
-
-
-
-
-
-
-'''
-def make_land_1km_cells():
-    'Make 1km grid cells that intersect land area of Iceland and save to geopackage.'
-
-    # extent of iceland
-    xmin, ymin, xmax, ymax = 2600000, 4700000, 3400000, 5300000
-
-    # load iceland land area geometries from geopackage
-    land_geometry = gpd.read_file(workspace + 'land_100k_decomposed.gpkg')
-    land_geometry = land_geometry.geometry
-    # build spatial index
-    lg_index = index.Index()
-    for i,g in enumerate(land_geometry): lg_index.insert(i, g.bounds)
-
-
-    output_cells = []
-    for x in range(xmin, xmax, 1000):
-        print(x)
-        for y in range(ymin, ymax, 1000):
-
-            # get items using spatial index
-            land_ = list(lg_index.intersection( (x, y, x + 1000, y + 1000) ))
-            if len(land_) == 0: continue
-
-            # make list of geometries from land_ids
-            land = []
-            for id in land_: land.append(land_geometry[id])
-            # make union of land geometries
-            land = shapely.ops.unary_union(land)
-
-            # make cell geometry
-            cell = shapely.geometry.box(x, y, x + 1000, y + 1000)
-
-            cell = land.intersection(cell)
-            if cell.area <= 0: continue
-
-            inspire_id = f'CRS3035RES1000mN{int(y)}5{int(x)}'
-            #print(f'Cell at ({x}, {y}) intersects land area with geometry: {intersection.area}')
-            output_cells.append( { "geometry": cell, "GRD_ID": inspire_id, } )
-    # save output cells to geopackage
-    output_gdf = gpd.GeoDataFrame(output_cells, geometry='geometry', crs='EPSG:3035')
-    output_gdf.to_file(workspace + 'out/land_1km_cells.gpkg', driver='GPKG')
-'''
-
-
-
-'''
-# nearest neighbor resampling
-def nn_resample(src, dst, scale):
-    'Nearest neighbor resampling from src to dst with given scale factor.'
-
-    src_h, src_w = src.shape
-    dst_h, dst_w = dst.shape
-
-    for i in range(dst_h):
-        for j in range(dst_w):
-            src_i = min(int(i * scale), src_h - 1)
-            src_j = min(int(j * scale), src_w - 1)
-            dst[i, j] = src[src_i, src_j]
-
-    return dst
-
-
-# load grid data from tiff file
-def load_grid_data(tiff_path):  
-    'Load grid data from a tiff file and return as a numpy array.'
-
-    with rasterio.open(tiff_path) as src:
-        data = src.read(1)  # Read the first band
-
-        # convert pixels to shapely geometries
-        array = np.array(data)
-'''
 
