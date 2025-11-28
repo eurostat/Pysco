27a3649a0c803beb681ee0d08d45333fd53d5169
jgaffuri <jgaf@proton.me>
2025-11-28 17:31:52 +0100

    a

--- src/census2021/resampling/process.py
+++ src/census2021/resampling/process.py
@@ -285,7 +285,6 @@ w = '/home/juju/gisco/census_2021_iceland/'
 pop_structure = { "sex" : ["sex_1", "sex_2"], "age_g" : ["age_g_1","age_g_2","age_g_3"], "pob_l" : ["pob_l_1","pob_l_2_1","pob_l_2_2"], "roy" : ["roy_1","roy_2_1","roy_2_2"] }
 
 print("Dasymetric disaggregation step 1")
-'''
 dasymetric_disaggregation_step_1(
     w+"IS_pop_grid_surf_3035.gpkg",
     w+"IS_pop_grid_surf_3035.gpkg", # strandlina_flakar_3035_decomposed clc_urban
@@ -304,7 +303,6 @@ dasymetric_disaggregation_step_1(
     structure= structure,
     pop_grouping_threshold= 10,
 )
-'''
 dasymetric_disaggregation_step_1(
     w+"IS_pop_grid_surf_3035_land.gpkg",
     w+"ghsl_land_3035.gpkg", # strandlina_flakar_3035_decomposed clc_urban
@@ -317,15 +315,14 @@ dasymetric_disaggregation_step_1(
 
 
 print("Dasymetric aggregation step 2")
-#dasymetric_aggregation_step_2(w+"out/disag_area.gpkg", w+"out/area_weighted.gpkg", tot_pop_att = "sex_0", structure=structure)
-#dasymetric_aggregation_step_2(w+"out/disag_area_land.gpkg", w+"out/dasymetric_land.gpkg", tot_pop_att = "sex_0", structure=structure)
+dasymetric_aggregation_step_2(w+"out/disag_area.gpkg", w+"out/area_weighted.gpkg", tot_pop_att = "sex_0", structure=structure)
+dasymetric_aggregation_step_2(w+"out/disag_area_land.gpkg", w+"out/dasymetric_land.gpkg", tot_pop_att = "sex_0", structure=structure)
 dasymetric_aggregation_step_2(w+"out/disag_area_ghsl_land.gpkg", w+"out/dasymetric_GHSL_land.gpkg", tot_pop_att = "sex_0", pop_structure=pop_structure)
 
 print("Dasymetric aggregation step 2 from points")
-#dasymetric_aggregation_step_2(w+"out/disag_point.gpkg", w+"out/area_weighted_rounded.gpkg", tot_pop_att = "sex_0", structure=structure, type='population')
-#dasymetric_aggregation_step_2(w+"out/disag_point_land.gpkg", w+"out/dasymetric_land_rounded.gpkg", tot_pop_att = "sex_0", structure=structure, type='population')
+dasymetric_aggregation_step_2(w+"out/disag_point.gpkg", w+"out/area_weighted_rounded.gpkg", tot_pop_att = "sex_0", structure=structure, type='population')
+dasymetric_aggregation_step_2(w+"out/disag_point_land.gpkg", w+"out/dasymetric_land_rounded.gpkg", tot_pop_att = "sex_0", structure=structure, type='population')
 dasymetric_aggregation_step_2(w+"out/disag_point_ghsl_land.gpkg", w+"out/dasymetric_GHSL_land_rounded.gpkg", tot_pop_att = "sex_0", pop_structure=pop_structure, type='population')
 
-#print("Nearest neighbour")
-#dasymetric_aggregation_step_2(w+"IS_pop_grid_point_3035.gpkg", "sex_0", w+"out/nearest_neighbour.gpkg", type='point', pop_atts=pop_atts)
-
+print("Nearest neighbour")
+dasymetric_aggregation_step_2(w+"IS_pop_grid_point_3035.gpkg", "sex_0", w+"out/nearest_neighbour.gpkg", type='point', pop_atts=pop_atts)
