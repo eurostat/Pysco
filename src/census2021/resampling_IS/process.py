7eb8b35a108528fe9b09687640697b8c21169944
jgaffuri <jgaf@proton.me>
2025-11-29 22:30:18 +0100

    a

--- src/census2021/resampling_IS/process.py
+++ src/census2021/resampling_IS/process.py
@@ -24,7 +24,7 @@ print("Dasymetric disaggregation step 1")
 dasymetric_disaggregation_step_1(
     w+"IS_pop_grid_surf_3035.gpkg",
     w+"IS_pop_grid_surf_3035.gpkg",
-    w+"out/disag_area.gpkg",
+    output_gpkg= w+"out/disag_area.gpkg",
     output_synthetic_population_gpkg= w+"out/disag_point.gpkg",
     tot_pop_att= "sex_0",
     pop_structure= pop_structure,
@@ -33,7 +33,7 @@ dasymetric_disaggregation_step_1(
 dasymetric_disaggregation_step_1(
     w+"IS_pop_grid_surf_3035_land.gpkg",
     w+"strandlina_flakar_3035_decomposed.gpkg",
-    w+"out/disag_area_land.gpkg",
+    output_gpkg= w+"out/disag_area_land.gpkg",
     output_synthetic_population_gpkg= w+"out/disag_point_land.gpkg",
     tot_pop_att= "sex_0",
     pop_structure= pop_structure,
@@ -42,7 +42,7 @@ dasymetric_disaggregation_step_1(
 dasymetric_disaggregation_step_1(
     w+"IS_pop_grid_surf_3035_land.gpkg",
     w+"ghsl_land_3035.gpkg",
-    w+"out/disag_area_ghsl_land.gpkg",
+    output_gpkg= w+"out/disag_area_ghsl_land.gpkg",
     output_synthetic_population_gpkg= w+"out/disag_point_ghsl_land.gpkg",
     tot_pop_att= "sex_0",
     pop_structure= pop_structure,
