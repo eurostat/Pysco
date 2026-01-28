40e98280bccbd08caa19d9ba4360e13c8e156861
jgaffuri <jgaf@proton.me>
2026-01-28 20:06:22 +0100

    upgrade

--- src/accessibility/euro_access.py
+++ src/accessibility/euro_access.py
@@ -7,11 +7,10 @@ from utils.featureutils import iter_features
 from utils.tomtomutils import weight_function, is_not_snappable_fun, initial_node_level_fun, final_node_level_fun, is_start_blocked, is_end_blocked
 
 #TODO
-# connex component issue:
-# detect connex components
+# ensures all POIs are connected to a large cc when going out
 # either remove the CCs, OR reconnect them, OR ensure it is not deconnected
-# secondary services accessibility
-# accessibility walking, cycling ?
+# accessibility to schools by walking
+# secondary education services accessibility
 
 
 # folders where to find the inputs
@@ -34,20 +33,15 @@ for grid_resolution in [100]: # 1000
     for service in ["healthcare", "education"]:
         for year in ["2023", "2020"]: #"2023"
 
+            # define tomtom year
+            tomtom_year = "2019" if year == "2020" else year
+
             def cell_id_fun(x,y): return "CRS3035RES"+str(grid_resolution)+"mN"+str(int(y))+"E"+str(int(x))
             def duration_simplification_fun(x): return int(round(x))
-
-            # choose number of processors, depending on service type and resolution
-            if grid_resolution == 100:
-                num_processors_to_use = 6 if service == "education" else 3 #2
-            else: num_processors_to_use = 10
 
             # define and create ouput folder, depending on year, service, resolution
             out_folder_service_year = out_folder + "out_" + service + "_" + year + "_" + str(grid_resolution) + "m/"
             os.makedirs(out_folder_service_year, exist_ok=True)
-
-            # define tomtom year
-            tomtom_year = "2019" if year == "2020" else year
 
             # define tomtom and POI loaders
             def road_network_loader(bbox): return iter_features(tomtom_data_folder + "tomtom"+tomtom_year+"12.gpkg", bbox=bbox) #, where="FOW!='20'"
@@ -69,13 +63,13 @@ for grid_resolution in [100]: # 1000
                 cell_id_fun = cell_id_fun,
                 grid_resolution= grid_resolution,
                 cell_network_max_distance= 1500,
-                to_network_speed_ms= 15 /3.6, # 15km/h by car
+                to_network_speed_ms= 15 /3.6,
                 file_size = 200000 if grid_resolution == 100 else 500000,
                 extention_buffer = 20000 if service=="education" else 60000,
                 detailled = True,
                 densification_distance = grid_resolution,
                 duration_simplification_fun = duration_simplification_fun,
-                num_processors = num_processors_to_use,
+                num_processors = 6 if service == "education" else 2,
                 shuffle=True,
                 show_detailled_messages = False
             )
