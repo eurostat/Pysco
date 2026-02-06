0446083bf751e2c252ae5aaa11e46f624e9fa025
jgaffuri <jgaf@proton.me>
2026-02-06 11:40:09 +0100

    upgrade

--- src/tesselation_validation/EBM.py
+++ src/tesselation_validation/EBM.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+from datetime import datetime
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from tesselation_validation.validation import validate_polygonal_tesselation
 
@@ -13,20 +14,23 @@ os.makedirs(out_folder, exist_ok=True)
 
 
 for case in ["A", "NUTS_1", "NUTS_2", "NUTS_3", "LAU"]:
-    print("*******", case)
+    print(datetime.now(), "*******", case)
 
+    print(datetime.now(), "check_ogc_validity")
     validate_polygonal_tesselation(
                 folder + "EBM_"+case+"_"+version+".gpkg",
                 out_folder + "issues_"+case+"_"+version+"_check_ogc_validity.gpkg",
                 check_ogc_validity=True,
                 )
 
+    print(datetime.now(), "check_intersection")
     validate_polygonal_tesselation(
                 folder + "EBM_"+case+"_"+version+".gpkg",
                 out_folder + "issues_"+case+"_"+version+"_check_intersection.gpkg",
                 check_intersection=True,
                 )
 
+    print(datetime.now(), "check_microscopic_segments")
     validate_polygonal_tesselation(
                 folder + "EBM_"+case+"_"+version+".gpkg",
                 out_folder + "issues_"+case+"_"+version+"_check_microscopic_segments.gpkg",
@@ -34,19 +38,23 @@ for case in ["A", "NUTS_1", "NUTS_2", "NUTS_3", "LAU"]:
                 microscopic_segment_threshold=0.1,
                 )
 
+    print(datetime.now(), "check_noding_issues")
     validate_polygonal_tesselation(
                 folder + "EBM_"+case+"_"+version+".gpkg",
                 out_folder + "issues_"+case+"_"+version+"_check_noding_issues.gpkg",
                 check_noding_issues=True,
                 node_to_segment_distance_threshold=0.5,
                 )
-            
+
+    print(datetime.now(), "check_thin_parts")
     validate_polygonal_tesselation(
                 folder + "EBM_"+case+"_"+version+".gpkg",
                 out_folder + "issues_"+case+"_"+version+"_check_thin_parts.gpkg",
                 check_thin_parts=True,
                 thin_part_threshold=0.1,
                 )
+
+    print(datetime.now(), "check_polygonisation")
     validate_polygonal_tesselation(
                 folder + "EBM_"+case+"_"+version+".gpkg",
                 out_folder + "issues_"+case+"_"+version+"_check_polygonisation.gpkg",
