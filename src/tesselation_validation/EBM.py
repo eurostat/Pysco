ab76322e3742f5f17ee7fbe39b1cf17acd4c1c04
jgaffuri <jgaf@proton.me>
2026-02-07 10:09:35 +0100

    a

--- src/tesselation_validation/EBM.py
+++ src/tesselation_validation/EBM.py
@@ -12,6 +12,8 @@ version = "2026"
 out_folder = "/home/juju/Bureau/EBM_"+version+"_validation/"
 os.makedirs(out_folder, exist_ok=True)
 
+
+# check geometry types
 if True:
     for case in ["A", "NUTS_1", "NUTS_2", "NUTS_3", "LAU"]:
         print(datetime.now(), "*******", case)
