2cbb37d9cc0ae2ae8e25b59a37008d0488b6ca4a
jgaffuri <jgaf@proton.me>
2026-04-21 13:58:43 +0200

    a

--- src/utils/grid2stat.py
+++ src/utils/grid2stat.py
@@ -117,6 +117,7 @@ def grid2stat(tiffs, stat_gpkg, stat_id, out_csv, out_dict=None, verbose=False):
 grid2stat(
         [
             [ "/home/juju/gisco/accessibility/gridviz/euro_access_evcp_2025_1000m_v2026_03.tif", 1 ],
+            [ "/home/juju/gisco/accessibility/gridviz/euro_access_evcp_2025_1000m_v2026_03.tif", 2 ],
             #[ "/home/juju/gisco/accessibility/euro_access_evcp_2025_1000m_v2026_03.tif" , 1 ],
             #[ "/home/juju/gisco/accessibility/euro_access_evcp_2025_1000m_v2026_03.tif" , 2 ]
         ],
