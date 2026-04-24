11e09a2f90bd0926c44d606d9901df0b679d97bb
jgaffuri <jgaf@proton.me>
2026-04-24 18:58:46 +0200

    a

--- src/accessibility/gridviz/service_points.py
+++ src/accessibility/gridviz/service_points.py
@@ -1,11 +1,22 @@
 
 
+import os
+
+from utils.gridutils import gpkg_point_to_csv
+
+#
 services_path = "/home/juju/geodata/gisco/basic_services/"
 version_tag = "v20260421"
 
+
+
+if not os.path.exists("tmp/"): os.makedirs("tmp/")
 for service in ["education","healthcare"]:
     for year in ["2020", "2023"]:
-        path = services_path + service + "_" + year + "_3035_" + version_tag + ".gpkg"
+        print(service, year)
 
-        #open gpkg file and save as csv, keeping only the column "name" and "geometry"
-        
+        gpkg_point_to_csv(services_path + service + "_" + year + "_3035_" + version_tag + ".gpkg",
+                          "tmp/" + service + "_" + year + "_3035_" + version_tag + ".csv",
+                          attributes_to_keep=["name"],
+                          rounding_precision=0)
+
