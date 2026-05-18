8e83b9a0c2c8c673538afc6b939424752966f27d
jgaffuri <jgaf@proton.me>
2026-05-18 15:13:14 +0200

    a

---
+++ src/census2021/hannes_prod_validation/compare.py
@@ -0,0 +1,21 @@
+# compare V2 gpkg with clean production gpkg
+
+
+# detect cells missing in V2 but present in production
+import geopandas as gpd
+
+prod = gpd.read_file("/home/juju/gisco/census_2021_production/census_grid_2021.gpkg")
+v2 = gpd.read_file("/home/juju/geodata/census/2021/ESTAT_Census_2021_V2.gpkg")
+
+prod_cells = set(prod["cell_id"])
+v2_cells = set(v2["cell_id"])
+missing_in_v2 = prod_cells - v2_cells
+missing_in_prod = v2_cells - prod_cells
+
+
+# save missing cells to gpkg
+missing_in_v2_gdf = prod[prod["cell_id"].isin(missing_in_v2)]
+missing_in_prod_gdf = v2[v2["cell_id"].isin(missing_in_prod)]
+missing_in_v2_gdf.to_file("/home/juju/gisco/census_2021_validation/geodiff/missing_in_v2.gpkg", driver="GPKG")
+missing_in_prod_gdf.to_file("/home/juju/gisco/census_2021_validation/geodiff/missing_in_prod.gpkg", driver="GPKG")
+
