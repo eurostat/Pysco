5a407a6d70243157baa30b47e215c379bdde6d5d
jgaffuri <jgaf@proton.me>
2026-06-05 16:54:13 +0200

    evrp points

--- src/accessibility/gridviz/service_points.py
+++ src/accessibility/gridviz/service_points.py
@@ -6,7 +6,7 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 from utils.gridutils import gpkg_point_to_csv
 
-prepare_csv = False
+prepare_csv = True
 aggregate = True
 tiling = True
 
