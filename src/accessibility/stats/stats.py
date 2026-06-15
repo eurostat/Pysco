d43c94e75ce984c834d3a8b02ac423a3b2ef17e9
jgaffuri <jgaf@proton.me>
2026-06-15 17:06:29 +0200

    accessibility stats computation

--- src/accessibility/stats/stats.py
+++ src/accessibility/stats/stats.py
@@ -18,7 +18,7 @@ print("Starting aggregation at: ", datetime.now())
 aggregate_geotiff_to_regions(
     gpkg_path=f"/home/juju/geodata/gisco/NUTS_RG_100K_2024_3035.gpkg",
     region_id_attr="NUTS_ID",
-    geotiff_path=f"/home/juju/gisco/accessibility/euro_access_evrp_2025_1000m_v2026_06.tif",
+    geotiff_path=f"/home/juju/gisco/accessibility/euro_access_evrp_2025_100m_v2026_06.tif",
     output_csv_path=f"/home/juju/gisco/accessibility/stats/evrp_2025_v2026_06.csv",
     output_col_name="evrp_2025",
 )
