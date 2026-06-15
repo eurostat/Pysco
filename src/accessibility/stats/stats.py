cd0e807836e29aa3bd5e4e3d18701ce3a7c075d7
jgaffuri <jgaf@proton.me>
2026-06-15 16:46:49 +0200

    accessibility stats computation

--- src/accessibility/stats/stats.py
+++ src/accessibility/stats/stats.py
@@ -1,18 +1,18 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
-from utils.featureutils import iter_features
+#from utils.featureutils import iter_features
 from utils.aggregate_geotiff import aggregate_geotiff_to_regions
 
 
 # see /utils/aggregate_geotiff.py
 
 nuts_version = "2024"
+
 #nuts_lvl = "3"
 #su = iter_features(f"/home/juju/geodata/gisco/CNTR_RG_100K_{nuts_version}_3035.gpkg", where="STAT_LEVL_CODE = " + str(nuts_lvl))
-
-su = list(su)
-print("Number of features: ", len(su))
+#su = list(su)
+#print("Number of features: ", len(su))
 
 aggregate_geotiff_to_regions(
     gpkg_path=f"/home/juju/geodata/gisco/CNTR_RG_100K_{nuts_version}_3035.gpkg",
