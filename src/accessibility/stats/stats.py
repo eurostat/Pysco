28711da6cc5406e087672272640dda1b873adeb4
jgaffuri <jgaf@proton.me>
2026-06-15 17:04:24 +0200

    accessibility stats computation

--- src/accessibility/stats/stats.py
+++ src/accessibility/stats/stats.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 import sys
 import os
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 #from utils.featureutils import iter_features
 from utils.aggregate_geotiff import aggregate_geotiff_to_regions
@@ -11,7 +13,7 @@ from utils.aggregate_geotiff import aggregate_geotiff_to_regions
 
 
 
-
+print("Starting aggregation at: ", datetime.now())
 
 aggregate_geotiff_to_regions(
     gpkg_path=f"/home/juju/geodata/gisco/NUTS_RG_100K_2024_3035.gpkg",
@@ -20,3 +22,5 @@ aggregate_geotiff_to_regions(
     output_csv_path=f"/home/juju/gisco/accessibility/stats/evrp_2025_v2026_06.csv",
     output_col_name="evrp_2025",
 )
+
+print("Finished aggregation at: ", datetime.now())
