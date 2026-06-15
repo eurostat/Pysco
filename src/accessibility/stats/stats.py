df0e07d55b4010592a3514dc0b4452b2f5a32e5c
jgaffuri <jgaf@proton.me>
2026-06-15 20:38:42 +0200

    accessibility stats computation

--- src/accessibility/stats/stats.py
+++ src/accessibility/stats/stats.py
@@ -6,12 +6,57 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")
 #from utils.featureutils import iter_features
 from utils.grid2stat import aggregate_geotiff_to_regions
 
+# produce population stats
+# make mask geotiffs
 # compute products
-# compute ratios of the results
-# compute nb services per LAU-NUTS - per category? check CHr file
+# join CSVs and compute ratios
 # map stats on nuts + LAUs (joe)
+# bonus: compute nb services per LAU-NUTS - per category? check CHr file
+
+produce_population_stats = True
 
 
+# output folder
+output_folder = "/home/juju/gisco/accessibility/stats/"
+# working folder
+working_folder = "./tmp/stats/"
+
+# the statistical units
+su = {
+    "LAU": { "path": "/home/juju/geodata/gisco/LAU_RG_100K_2024_3035.gpkg" , "id": "GISCO_ID" },
+    "NUTS": { "path": "/home/juju/geodata/gisco/NUTS_RG_100K_2024_3035.gpkg", "id": "NUTS_ID" }
+}
+
+# population rasters
+pop_rasters = {
+    "1000": "/home/juju/gisco/census_2021_v3_production/ESTAT_Census_2021_V3.tiff",
+    "100": "/home/juju/geodata/jrc/JRC_CENSUS_2021_100m_grid/JRC-CENSUS_2021_100m_new_bbox.tif"
+}
+
+
+# make folders
+os.makedirs(output_folder, exist_ok=True)
+os.makedirs(working_folder, exist_ok=True)
+
+
+# prepare total population per su
+if produce_population_stats:
+    for res in ["100", "1000"]:
+        for su_name, su_info in su.items():
+
+            print(datetime.now(), "produce T population from " +res+"m for "+su_name ) 
+            aggregate_geotiff_to_regions(
+                gpkg_path=su_info["path"],
+                region_id_attr=su_info["id"],
+                geotiff_path=pop_rasters[res],
+                output_csv_path=working_folder + f"population_T_{su_name}_{res}m.csv",
+                output_col_name="evrp_2025",
+            )
+    #TODO do other categories from 1000m
+
+
+
+'''
 print("Starting aggregation at: ", datetime.now())
 
 aggregate_geotiff_to_regions(
@@ -26,3 +71,4 @@ aggregate_geotiff_to_regions(
 
 print("Finished aggregation at: ", datetime.now())
 
+'''
