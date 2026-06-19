3015b7af7257de7ce863cb5f8b1e2052a2973eb2
jgaffuri <jgaf@proton.me>
2026-06-19 10:57:12 +0200

    accessibility stats computation

--- src/accessibility/stats/pivot.py
+++ src/accessibility/stats/pivot.py
@@ -7,8 +7,15 @@ from utils.csvutils import hypercube_csv_to_timeseries_csv
 
 
 in_folder = "/home/juju/gisco/accessibility/stats/"
+out_folder = in_folder + "decomposed/"
 service = "evrp"
 geo = "NUTS_2024"
-hypercube_csv_to_timeseries_csv(in_folder + "euro_access_"+service+"_"+geo+".csv", in_folder + "decomposed/",
-                    output_file_name_fun = lambda f: "euro_access_"+service+"_"+geo+"__"+f)
 
+hypercube_csv_to_timeseries_csv(
+    in_folder + "euro_access_"+service+"_"+geo+".csv",
+    out_folder,
+    output_file_name_fun = lambda f: "euro_access_"+service+"_"+geo+"__"+f)
+
+# delete NR files (not usefull)
+for filename in os.listdir(out_folder):
+    if "__UNIT_NR__" in filename: os.remove(os.path.join(out_folder, filename))
