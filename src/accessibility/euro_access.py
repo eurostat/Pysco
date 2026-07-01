8f80916376e9d9e69eb9b6fcb812953e032eeb67
jgaffuri <jgaf@proton.me>
2026-07-01 12:23:05 +0200

    reorganise

--- src/accessibility/euro_access.py
+++ src/accessibility/euro_access.py
@@ -6,14 +6,16 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from accessibility.step1_grid_computation import compute_accessibility_grids
 from accessibility.step2_combine_to_geotiff import combine_to_geotiff
-#from  import compute_accessibility_grids
+from accessibility.step3_gridviz_grids import gridviz_tiling
 #from 2_combine_to_geotiff import combine_to_geotiff
 
 # TODO
+# TODO review move
 # accessibility to schools by walking
 # secondary education services accessibility
 
 # Load parameters from JSON file
+# TODO as parameter
 params_file = '/home/juju/workspace/Pysco/src/accessibility/params_julien.json'
 with open(params_file, 'r') as f: params = json.load(f)
 
@@ -23,3 +25,5 @@ compute_accessibility_grids(params)
 # 2
 combine_to_geotiff(params, do_combination=True, resolutions=[100])
 
+# 3
+gridviz_tiling(params, aggregate=True, tiling=True, zip_move=True)
--- src/accessibility/step3_gridviz_grids.py
+++ src/accessibility/step3_gridviz_grids.py
@@ -3,88 +3,83 @@
 from pygridmap import gridtiler_raster
 import sys
 import os
-import json
 import shutil
 from rasterio.enums import Resampling
 from datetime import datetime
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from utils.geotiff import resample_geotiff_aligned
 
-# Load parameters from JSON file
-params_file = '/home/juju/workspace/Pysco/src/accessibility/params_julien.json'
-with open(params_file, 'r') as f: params = json.load(f)
 
 
-aggregate = True
-tiling = True
-zip_move = True
+def gridviz_tiling(params, services=None, aggregate=True, tiling=True, zip_move=True):
 
-get_k = lambda service: 5 if service == "evrp" else 3
+    if services is None: services = params["pois_datasets"].keys()
+    get_k = lambda service: 5 if service == "evrp" else 3
 
-resolutions = [ 100000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100 ]
+    resolutions = [ 100000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100 ]
 
-folder_gridviz = params["out_folder"] + "gridviz/"
-if not os.path.exists(folder_gridviz): os.makedirs(folder_gridviz)
+    folder_gridviz = params["out_folder"] + "gridviz/"
+    if not os.path.exists(folder_gridviz): os.makedirs(folder_gridviz)
 
-# aggregate at various resolutions - median
-if aggregate:
-    print(datetime.now(), "aggregate")
-    for service in ["evrp"]: #dataset_versions.keys():
-        for year in params["dataset_versions"][service].keys():
-
-            # it is better to resample all resolution from 100m one. Otherwise, we do medians of medians which may create some biais around places with many nodata pixels
-            for resolution in resolutions:
-                print(datetime.now(), service, year, resolution)
-                resample_geotiff_aligned(params["out_folder"] + "euro_access_"+service+"_"+year+"_100m_"+params["dataset_versions"][service][year]+".tif",
-                                         folder_gridviz+"euro_access_"+service+"_" + year+"_"+str(resolution) + "m_"+params["dataset_versions"][service][year]+".tif",
-                                         resolution, Resampling.med)
-
-
-if tiling:
-    print(datetime.now(), "tiling")
-    for resolution in resolutions:
-        for service in ["evrp"]: #dataset_versions.keys():
-
-            print(datetime.now(), "Tiling", service, resolution)
-
-            # make folder for resolution
-            folder_ = folder_gridviz + service + "/" + str(resolution) + "/"
-            if not os.path.exists(folder_): os.makedirs(folder_)
-
-            # prepare dict for geotiff bands
-            dict = {}
-            k = get_k(service)
+    # aggregate at various resolutions - median
+    if aggregate:
+        print(datetime.now(), "aggregate")
+        for service in services:
             for year in params["dataset_versions"][service].keys():
-                dict["dt_1_" + year] = {"file":folder_gridviz+"euro_access_"+service+"_"+year+"_"+str(resolution)+"m_"+params["dataset_versions"][service][year]+".tif", "band":1}
-                dict["dt_a"+str(k)+"_" + year] = {"file":folder_gridviz+"euro_access_"+service+"_"+year+"_"+str(resolution)+"m_"+params["dataset_versions"][service][year]+".tif", "band":2}
-                dict["POP_2021"] = { "file":params["folder_pop_tiff"]+"pop_2021_"+str(resolution)+".tif", "band":1 }
 
-            # launch tiling
-            gridtiler_raster.tiling_raster(
-                dict,
-                folder_,
-                crs="EPSG:3035",
-                tile_size_cell = 256,
-                format="parquet",
-                num_processors_to_use = 10,
-                modif_fun = round,
-                )
-            
-if zip_move:
-    # move/copy tiffs
-    for service in ["evrp"]: ##dataset_versions.keys():
+                # it is better to resample all resolution from 100m one. Otherwise, we do medians of medians which may create some biais around places with many nodata pixels
+                for resolution in resolutions:
+                    print(datetime.now(), service, year, resolution)
+                    resample_geotiff_aligned(params["out_folder"] + "euro_access_"+service+"_"+year+"_100m_"+params["dataset_versions"][service][year]+".tif",
+                                            folder_gridviz+"euro_access_"+service+"_" + year+"_"+str(resolution) + "m_"+params["dataset_versions"][service][year]+".tif",
+                                            resolution, Resampling.med)
 
-        # zip and move tiles
-        print(datetime.now(), "Zip tiles", service)
-        shutil.make_archive(folder_gridviz + service, "zip", folder_gridviz + service + "/")
-        print(datetime.now(), "Move zip file", service)
-        shutil.move(folder_gridviz + service + ".zip", params["target_folder"])
 
-        for year in params["dataset_versions"][service].keys():
-            print(datetime.now(), "Copy tiff files", service, year)
+    if tiling:
+        print(datetime.now(), "tiling")
+        for resolution in resolutions:
+            for service in services:
 
-            # 100m
-            shutil.copy(params["out_folder"]+"euro_access_"+service+"_"+year+"_100m_"+params["dataset_versions"][service][year]+".tif", params["target_folder"])
-            # 1000m
-            shutil.copy(folder_gridviz+"euro_access_"+service+"_"+year+"_1000m_"+params["dataset_versions"][service][year]+".tif", params["out_folder"])
-            shutil.copy(params["out_folder"]+"euro_access_"+service+"_"+year+"_1000m_"+params["dataset_versions"][service][year]+".tif", params["target_folder"])
+                print(datetime.now(), "Tiling", service, resolution)
+
+                # make folder for resolution
+                folder_ = folder_gridviz + service + "/" + str(resolution) + "/"
+                if not os.path.exists(folder_): os.makedirs(folder_)
+
+                # prepare dict for geotiff bands
+                dict = {}
+                k = get_k(service)
+                for year in params["dataset_versions"][service].keys():
+                    dict["dt_1_" + year] = {"file":folder_gridviz+"euro_access_"+service+"_"+year+"_"+str(resolution)+"m_"+params["dataset_versions"][service][year]+".tif", "band":1}
+                    dict["dt_a"+str(k)+"_" + year] = {"file":folder_gridviz+"euro_access_"+service+"_"+year+"_"+str(resolution)+"m_"+params["dataset_versions"][service][year]+".tif", "band":2}
+                    dict["POP_2021"] = { "file":params["folder_pop_tiff"]+"pop_2021_"+str(resolution)+".tif", "band":1 }
+
+                # launch tiling
+                gridtiler_raster.tiling_raster(
+                    dict,
+                    folder_,
+                    crs="EPSG:3035",
+                    tile_size_cell = 256,
+                    format="parquet",
+                    num_processors_to_use = 10,
+                    modif_fun = round,
+                    )
+                
+    if zip_move:
+        # move/copy tiffs
+        for service in services:
+
+            # zip and move tiles
+            print(datetime.now(), "Zip tiles", service)
+            shutil.make_archive(folder_gridviz + service, "zip", folder_gridviz + service + "/")
+            print(datetime.now(), "Move zip file", service)
+            shutil.move(folder_gridviz + service + ".zip", params["target_folder"])
+
+            for year in params["dataset_versions"][service].keys():
+                print(datetime.now(), "Copy tiff files", service, year)
+
+                # 100m
+                shutil.copy(params["out_folder"]+"euro_access_"+service+"_"+year+"_100m_"+params["dataset_versions"][service][year]+".tif", params["target_folder"])
+                # 1000m
+                shutil.copy(folder_gridviz+"euro_access_"+service+"_"+year+"_1000m_"+params["dataset_versions"][service][year]+".tif", params["out_folder"])
+                shutil.copy(params["out_folder"]+"euro_access_"+service+"_"+year+"_1000m_"+params["dataset_versions"][service][year]+".tif", params["target_folder"])
