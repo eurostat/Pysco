d363af96c084694f98a33efe56a7517895f58931
jgaffuri <jgaf@proton.me>
2026-07-08 10:31:59 +0200

    a

--- src/z/tiff.py
+++ src/z/tiff.py
@@ -1,11 +1,13 @@
 import sys
 import os
+import numpy as np
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from utils.geotiff import crop_extend_bbox
+from utils.geotiff import crop_extend_bbox, resample_geotiff_aligned, Resampling
 from utils.convert import gpkg_grid_to_geotiff
 
 
-if True:
+# pop grid gpkg to geotiff
+if False:
     gpkg_grid_to_geotiff(
         [ "/home/juju/geodata/gisco/grids/grid_1km_surf.gpkg" ],
         "/home/juju/geodata/census/pop_20XX_1000m.tif",
@@ -16,6 +18,17 @@ if True:
         dtype="int32",
         tiff_nodata_value=-9999
     )
+
+# resample population geotiffs at different resolutions
+if True:
+    for resolution in [2000,5000,10000,20000,50000,100000]:
+        print(f"Resampling to {resolution}m...")
+        resample_geotiff_aligned("/home/juju/geodata/census/pop_20XX_1000m.tif",
+                                "/home/juju/geodata/census/pop_20XX_"+str(resolution)+"m.tif",
+                                resolution,
+                                resampling=Resampling.sum,
+                                dtype=np.int32,
+                                )
 
 
 
